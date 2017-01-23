7922f32c54aee2dec3c89dd20e8b10e2ba54211b
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-01-23 22:04:45 +0000

    WIP: actually try to push to gitlab_repo

--- terraform/scripts/commit-tfstate.sh
+++ terraform/scripts/commit-tfstate.sh
@@ -16,7 +16,7 @@ if [ -n "${status}" ]; then
     echo "Pushing to ${GITHUB_REPO}..."
     subrepos/gitlab-ci-git-push/git-push ${GITHUB_REPO} ${CI_BUILD_REF_NAME} || (echo "Failed to push to github" && exit 1)
     echo "Pushing to ${GITLAB_REPO}..."
-    subrepos/gitlab-ci-git-push/git-push ${CI_BUILD_REPO} ${CI_BUILD_REF_NAME} || (echo "Failed to push to gitlab" && exit 1)
+    subrepos/gitlab-ci-git-push/git-push ${GITLAB_REPO} ${CI_BUILD_REF_NAME} || (echo "Failed to push to gitlab" && exit 1)
 else
     echo "No changes to terraform state"
 fi
