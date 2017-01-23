e0127965393f43a04109fc14fb14af9ff0b20bc0
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-01-23 21:46:01 +0000

    move tfstate commit to a script

---
+++ terraform/scripts/commit-tfstate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+branch=$1
+
+git checkout -b ${branch}
+git add terraform/terraform.tfstate
+
+status=$(git status --porcelain | awk '$1!="??"')
+
+if [ -n "${status}" ]; then
+    git commit -m "Changes to terraform.tfstate made by terraform" || (echo "Failed to commit changes to terraform.tfstate" && exit 1)
+    subrepos/gitlab-ci-git-push/git-push ${GITHUB_REPO} ${CI_BUILD_REF_NAME} || (echo "Failed to push to github" && exit 1)
+    subrepos/gitlab-ci-git-push/git-push ${CI_BUILD_REPO} ${CI_BUILD_REF_NAME} || (echo "Failed to push to gitlab" && exit 1)
+else
+    echo "No changes to terraform state"
+fi
+
