385bfc20ae780e13f01aa8f5278774431bc1819e
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-01-23 22:49:16 +0000

    reduce down to 3 consul servers

--- terraform/consul.tf
+++ terraform/consul.tf
@@ -4,7 +4,7 @@ resource "openstack_compute_instance_v2" "consul-servers" {
   flavor_name = "m1.small"
   key_pair = "${openstack_compute_keypair_v2.mercury.id}"
   security_groups = ["${openstack_compute_secgroup_v2.ssh.id}"]
-  count = 5
+  count = 3
 }
 
 output "consul_ips" {
