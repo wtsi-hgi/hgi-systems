18828b09f100d849b306693ca96315e4fd3347c8
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-01-23 21:05:33 +0000

    increase count of consul servers to 3

--- terraform/consul.tf
+++ terraform/consul.tf
@@ -4,6 +4,7 @@ resource "openstack_compute_instance_v2" "consul-servers" {
   flavor_name = "m1.small"
   key_pair = "${openstack_compute_keypair_v2.mercury.id}"
   security_groups = ["${openstack_compute_secgroup_v2.ssh.id}"]
+  count = 3
 }
 
 output "consul-addresses" {
