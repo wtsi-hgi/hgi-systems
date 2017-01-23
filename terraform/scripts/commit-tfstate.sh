c101ccdecbdb3d14e842118259c17cc61aa7753d
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-01-23 22:10:50 +0000

    add commit tag to avoid CI runs when committing tfstate changes

--- terraform/scripts/commit-tfstate.sh
+++ terraform/scripts/commit-tfstate.sh
@@ -12,7 +12,7 @@ git add terraform/terraform.tfstate
 status=$(git status --porcelain | awk '$1!="??"')
 
 if [ -n "${status}" ]; then
-    git commit -m "Changes to terraform.tfstate made by terraform" || (echo "Failed to commit changes to terraform.tfstate" && exit 1)
+    git commit -m "Changes to terraform.tfstate made by terraform [no ci]" || (echo "Failed to commit changes to terraform.tfstate" && exit 1)
     echo "Pushing to ${GITHUB_REPO}..."
     subrepos/gitlab-ci-git-push/git-push ${GITHUB_REPO} ${CI_BUILD_REF_NAME} || (echo "Failed to push to github" && exit 1)
     echo "Pushing to ${GITLAB_REPO}..."
