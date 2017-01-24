59155c1ad9933d4d1e05fd0a29748750095deb67
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-01-24 16:56:16 +0000

    update commit-tfstate.sh to take second argument with commit message

--- terraform/scripts/commit-tfstate.sh
+++ terraform/scripts/commit-tfstate.sh
@@ -3,6 +3,7 @@
 set -euf -o pipefail
 
 branch=$1
+commit_message=$2
 
 git config user.name "Mercury"
 git config user.email "mercury@sanger.ac.uk"
@@ -12,7 +13,7 @@ git add terraform/terraform.tfstate
 status=$(git status --porcelain | awk '$1!="??"')
 
 if [ -n "${status}" ]; then
-    git commit -m "Changes to terraform.tfstate made by terraform [no ci]" || (echo "Failed to commit changes to terraform.tfstate" && exit 1)
+    git commit -m "${commit_message}" || (echo "Failed to commit changes to terraform.tfstate" && exit 1)
     echo "Pushing to ${GITHUB_REPO}..."
     subrepos/gitlab-ci-git-push/git-push ${GITHUB_REPO} ${CI_BUILD_REF_NAME} || (echo "Failed to push to github" && exit 1)
     echo "Pushing to ${GITLAB_REPO}..."
