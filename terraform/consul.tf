956fc533863843dfbfeabee918c4ae757dc250ab
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-01-25 15:28:17 +0000

    remove extraneous brace from consul.tf

--- terraform/consul.tf
+++ terraform/consul.tf
@@ -7,7 +7,7 @@ resource "openstack_compute_instance_v2" "consul-servers" {
   key_pair = "${openstack_compute_keypair_v2.mercury_hgiarvados.id}"
   security_groups = ["${openstack_compute_secgroup_v2.ssh_hgiarvados.id}"]
   network {
-    id = "${openstack_networking_network_v2.main_hgiarvados.id}"]
+    id = "${openstack_networking_network_v2.main_hgiarvados.id}"
   }
 
   # wait for host to be available via ssh
