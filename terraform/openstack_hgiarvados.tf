18f22880c934ed8f1062057abe64871ebb8723bd
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-01-25 15:21:12 +0000

    add hgiarvados main network and assign consul servers to it explicitly

--- terraform/openstack_hgiarvados.tf
+++ terraform/openstack_hgiarvados.tf
@@ -27,3 +27,21 @@ resource "openstack_compute_secgroup_v2" "ssh_hgiarvados" {
   }
 }
 
+resource "openstack_networking_network_v2" "main_hgiarvados" {
+  provider = "openstack.hgiarvados"
+  name = "main_hgiarvados"
+  admin_state_up = "true"
+}
+
+resource "openstack_networking_subnet_v2" "main_hgiarvados" {
+  provider = "openstack.hgiarvados"
+  name = "main_hgiarvados"
+  network_id = "${openstack_networking_network_v2.main_hgiarvados.id}"
+  cidr = "10.101.0.0/24"
+  ip_version = 4
+}
+
+resource "openstack_networking_router_v2" "main_public_hgiarvados" {
+  name = "main_public_hgiarvados"
+  external_gateway = "9f50f282-2a4c-47da-88f8-c77b6655c7db"
+}
