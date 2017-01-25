f8b65b4b6383436346c5e970f459dc91157f9e63
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-01-25 14:58:14 +0000

    WIP: attempt to run ansible playbook on just-provisioned consul hosts

--- terraform/consul.tf
+++ terraform/consul.tf
@@ -1,11 +1,22 @@
 resource "openstack_compute_instance_v2" "consul-servers" {
+  count = 0
   provider = "openstack.hgiarvados"
   name = "consul-server-${count.index}"
   image_name = "${var.docker_image_name}"
   flavor_name = "m1.small"
   key_pair = "${openstack_compute_keypair_v2.mercury_hgiarvados.id}"
   security_groups = ["${openstack_compute_secgroup_v2.ssh_hgiarvados.id}"]
-  count = 0
+
+  # wait for host to be available via ssh
+  provisioner "remote-exec" {
+    inline = [
+      "hostname"
+    ]
+  }
+  # provision using ansible
+  provisioner "local-exec" {
+    command = "ANSIBLE_CONFIG ../ansible/ansible-minimal.cfg ansible-playbook -i ../ansible/production_hosts.d -l \"${join('', openstack_compute_instance_v2.consul-servers.*.name)}\" ../ansible/site.yml"
+  }
 }
 
 output "consul_ips" {
