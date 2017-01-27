20c1f668ecbd29129850a8cded39efdae855bd5c
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-01-27 14:52:03 +0000

    move current terraform work to terraform/production and adjust gitlab ci scripts

--- gitlab/commit-tfstate.sh
+++ gitlab/commit-tfstate.sh
@@ -4,16 +4,17 @@ set -euf -o pipefail
 
 branch=$1
 commit_message=$2
+tfstate_paths=$3
 
 git config user.name "Mercury"
 git config user.email "mercury@sanger.ac.uk"
 git checkout -b ${branch}
-git add terraform/terraform.tfstate
+git add "${tfstate_paths}"
 
 status=$(git status --porcelain | awk '$1!="??"')
 
 if [ -n "${status}" ]; then
-    git commit -m "${commit_message}" || (echo "Failed to commit changes to terraform.tfstate" && exit 1)
+    git commit -m "${commit_message}" || (echo "Failed to commit changes to ${tfstate_paths}" && exit 1)
     echo "Pushing to ${GITHUB_REPO}..."
     subrepos/gitlab-ci-git-push/git-push ${GITHUB_REPO} ${CI_BUILD_REF_NAME} || (echo "Failed to push to github" && exit 1)
     echo "Pushing to ${GITLAB_REPO}..."
