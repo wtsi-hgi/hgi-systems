87e962221ddf99cd4d5b9e5182666c9bf6d14c8c
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-01-29 16:47:11 +0000

    change paths to ansible

--- terraform/production/ssh-gateway.tf
+++ terraform/production/ssh-gateway.tf
@@ -31,7 +31,7 @@ resource "openstack_compute_instance_v2" "ssh-gateway-hgiarvados" {
   }
   # provision using ansible
   provisioner "local-exec" {
-    command = "ANSIBLE_CONFIG=../ansible/ansible-minimal.cfg ansible-playbook -i ../ansible/production_hosts.d -l ssh-gateway-hgiarvados ../ansible/site.yml"
+    command = "ANSIBLE_CONFIG=../../ansible/ansible-minimal.cfg ansible-playbook -i ../../ansible/production_hosts.d -l ssh-gateway-hgiarvados ../../ansible/site.yml"
   }
 }
 
@@ -74,7 +74,7 @@ resource "openstack_compute_instance_v2" "ssh-gateway-hgi" {
   }
   # provision using ansible
   provisioner "local-exec" {
-    command = "ANSIBLE_CONFIG=../ansible/ansible-minimal.cfg ansible-playbook -i ../ansible/production_hosts.d -l ssh-gateway-hgi ../ansible/site.yml"
+    command = "ANSIBLE_CONFIG=../../ansible/ansible-minimal.cfg ansible-playbook -i ../../ansible/production_hosts.d -l ssh-gateway-hgi ../../ansible/site.yml"
   }
 }
 
