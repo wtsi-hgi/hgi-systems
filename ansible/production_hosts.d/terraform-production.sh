8d6e7bd32c74f841a3e48fd174fdf4cb5423c4e4
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-02-09 01:59:36 +0000

    add additional ansible groups to terraform-production dynamic inventory

--- ansible/production_hosts.d/terraform-production.sh
+++ ansible/production_hosts.d/terraform-production.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-export TF_ANSIBLE_GROUPS_TEMPLATE='{{ ["all", "tf_provider_"+provider] | join("\n") }}'
+export TF_ANSIBLE_INVENTORY_NAME_TEMPLATE='{{ name }}'
+
+export TF_ANSIBLE_GROUPS_TEMPLATE='{{ ["all",
+"tf_provider_"+provider,
+"tf_type_"+type,
+"tf_name_"+primary.attributes.name] | join("\n") }}
+{% set newline = joiner("\n") -%}
+{% for security_group in primary.expanded_attributes.security_groups -%}
+{{ newline() }}tf_security_group_{{ security_group }}
+{%- endfor -%}
+{% for network in primary.expanded_attributes.network -%}
+{{ newline() }}tf_network_{{ network.name }}
+{%- endfor -%}'
+
 export TF_STATE=../../terraform/production/terraform.tfstate
+
 ../../subrepos/yatadis/yatadis.py $@
