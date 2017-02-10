24d05fb7f88bf39cb9b4feb54df7f8f1e3224d95
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-02-10 00:23:40 +0000

    tremove f_network _groups out of terraform-production.sh

--- ansible/production_hosts.d/terraform-production.sh
+++ ansible/production_hosts.d/terraform-production.sh
@@ -13,11 +13,23 @@ export TF_ANSIBLE_GROUPS_TEMPLATE=$(cat <<EOF
 {% for security_group in primary.expanded_attributes.security_groups -%}
 {{ newline() }}tf_security_group_{{ security_group }}
 {%- endfor -%}
-{% for network in primary.expanded_attributes.network -%}
-{{ newline() }}tf_network_{{ network.name }}
-{%- endfor -%}
 EOF
 )
+
+# export TF_ANSIBLE_GROUPS_TEMPLATE=$(cat <<EOF
+# {{ ["all",
+# "tf_provider_"+provider,
+# "tf_type_"+type,
+# "tf_name_"+primary.attributes.name] | join("\n") }}
+# {% set newline = joiner("\n") -%}
+# {% for security_group in primary.expanded_attributes.security_groups -%}
+# {{ newline() }}tf_security_group_{{ security_group }}
+# {%- endfor -%}
+# {% for network in primary.expanded_attributes.network -%}
+# {{ newline() }}tf_network_{{ network.name }}
+# {%- endfor -%}
+# EOF
+# )
 
 export TF_STATE="${terraform_dir}/production/terraform.tfstate"
 
