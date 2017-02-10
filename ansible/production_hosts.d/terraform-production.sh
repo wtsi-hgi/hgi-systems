beec873cd698c8cdc616d20fea1f6b4ebd885552
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-02-10 00:28:30 +0000

    remove tf_name_ group from terrafrom-production.sh inventory script

--- ansible/production_hosts.d/terraform-production.sh
+++ ansible/production_hosts.d/terraform-production.sh
@@ -7,8 +7,7 @@ export TF_ANSIBLE_INVENTORY_NAME_TEMPLATE='{{ name }}'
 export TF_ANSIBLE_GROUPS_TEMPLATE=$(cat <<EOF
 {{ ["all",
 "tf_provider_"+provider,
-"tf_type_"+type,
-"tf_name_"+primary.attributes.name] | join("\n") }}
+"tf_type_"+type] | join("\n") }}
 {% set newline = joiner("\n") -%}
 {% for security_group in primary.expanded_attributes.security_groups -%}
 {{ newline() }}tf_security_group_{{ security_group }}
