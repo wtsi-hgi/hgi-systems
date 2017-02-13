814f9e27dbb634c5b4e909520bd650eb62cd9a05
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-02-13 12:20:04 +0000

    move plan argument to end of terraform apply command line

--- gitlab/terraform-apply.sh
+++ gitlab/terraform-apply.sh
@@ -8,13 +8,16 @@ mkdir -p "${CI_PROJECT_DIR}/artifacts"
 
 cd "terraform/${REGION}"
 
-terraform apply -input=false plan -state-out="${ENV}.tfstate.txt"
-success=$?
+terraform apply -input=false -state-out="${ENV}.tfstate.txt" plan
+apply_exit_code=$?
 cp "${ENV}.tfstate" "${CI_PROJECT_DIR}/artifacts/"
 
-if [[ ${success} -eq 0 ]]; then
+if [[ ${apply_exit_code} -eq 0 ]]; then
     terraform output -json -state "${ENV}.tfstate" > output.json
     cp output.json "${CI_PROJECT_DIR}/artifacts/"
     terraform show -no-color > "${ENV}.tfstate.txt"
     cp "${ENV}.tfstate.txt" "${CI_PROJECT_DIR}/artifacts/"
+else
+    >&2 echo "terraform apply failed"
+    exit ${apply_exit_code}
 fi
