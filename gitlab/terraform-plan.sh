4cada69283aabf4ca6897e62bd0e902db63c16d4
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-02-13 14:32:06 +0000

    don't use wildcard to list artifacts

--- gitlab/terraform-plan.sh
+++ gitlab/terraform-plan.sh
@@ -2,9 +2,9 @@
 
 set -euf -o pipefail
 
-artifacts_dir="${CI_PROJECT_DIR}/artifacts/"
+artifacts_dir="${CI_PROJECT_DIR}/artifacts"
 echo "Listing contents of artifacts directory ${artifacts_dir}"
-artifacts=$(ls "${artifacts_dir}/"*)
+artifacts=$(ls "${artifacts_dir}/")
 
 echo "Copying ${artifacts} to terraform/${REGION}/"
 cp ${artifacts} "terraform/${REGION}/"
