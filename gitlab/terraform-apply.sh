5f4cb8cb835e08ab7cf84b1afe6ef61c00a4ad47
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-02-13 12:13:01 +0000

    move terraform apply inline script to gitlab/terraform-apply.sh

---
+++ gitlab/terraform-apply.sh
@@ -0,0 +1,18 @@
+cp ${CI_PROJECT_DIR}/artifacts/* "terraform/${REGION}/"
+rm -rf "${CI_PROJECT_DIR}/artifacts"
+mkdir -p "${CI_PROJECT_DIR}/artifacts"
+
+(echo "$ANSIBLE_VAULT_PASSWORD" > /tmp/ansible_vault.pw)
+
+cd "terraform/${REGION}"
+
+terraform apply -input=false plan
+success=$?
+cp "${ENV}.tfstate" "${CI_PROJECT_DIR}/artifacts/"
+
+if [[ ${success} -eq 0 ]]; then
+    terraform output -json -state "${ENV}.tfstate" > output.json
+    cp output.json "${CI_PROJECT_DIR}/artifacts/"
+    terraform show -no-color > "${ENV}.tfstate.txt"
+    cp "${ENV}.tfstate.txt" "${CI_PROJECT_DIR}/artifacts/"
+fi
