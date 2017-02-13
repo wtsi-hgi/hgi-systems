1fb4c102e455a81a1f1934c3130902a38930edf6
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-02-13 13:44:25 +0000

    improve logging from terraform-apply.sh

--- gitlab/terraform-apply.sh
+++ gitlab/terraform-apply.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
-cp ${CI_PROJECT_DIR}/artifacts/* "terraform/${REGION}/"
-rm -rf "${CI_PROJECT_DIR}/artifacts"
-mkdir -p "${CI_PROJECT_DIR}/artifacts"
+set -euf -o pipefail
 
+artifacts=$(ls ${CI_PROJECT_DIR}/artifacts/*)
+
+echo "Copying ${artifacts} to terraform/${REGION}/"
+cp ${artifacts} "terraform/${REGION}/"
+
+echo "Emptying artifacts directory"
+rm -rf "${CI_PROJECT_DIR}/artifacts" && mkdir -p "${CI_PROJECT_DIR}/artifacts"
+
+echo "Generating /tmp/ansible_vault.pw"
 (echo "$ANSIBLE_VAULT_PASSWORD" > /tmp/ansible_vault.pw)
 
 cd "terraform/${REGION}"
 
+echo "Calling terraform apply"
 terraform apply -input=false -state-out="${ENV}.tfstate.txt" plan
 apply_exit_code=$?
+
+echo "Copying ${ENV}.tfstate to artifacts"
 cp "${ENV}.tfstate" "${CI_PROJECT_DIR}/artifacts/"
 
 if [[ ${apply_exit_code} -eq 0 ]]; then
+    echo "terraform apply succeeded, generating output state artifacts"
     terraform output -json -state "${ENV}.tfstate" > output.json
     cp output.json "${CI_PROJECT_DIR}/artifacts/"
     terraform show -no-color > "${ENV}.tfstate.txt"
     cp "${ENV}.tfstate.txt" "${CI_PROJECT_DIR}/artifacts/"
 else
-    >&2 echo "terraform apply failed"
+    >&2 echo "terraform apply failed: ${apply_exit_code}"
     exit ${apply_exit_code}
 fi
