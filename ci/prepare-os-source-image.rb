1e3f788cd57405cd107be6053e2052c90a84db43
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-02-20 11:58:14 +0000

    Adds ability to specify multiple OS source image names (first that exists is used).

---
+++ ci/prepare-os-source-image.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require('fileutils')
+require('open3')
+
+OS_SOURCE_IMAGE_SEPARATOR = ','
+RESOURCE_NOT_FOUND_ERROR = 'Could not find resource'
+
+os_source_images = ENV['OS_SOURCE_IMAGE']
+os_source_image_artifact = ENV['GITLAB_OS_SOURCE_IMAGE_ARTIFACT']
+
+# Ensure artifact directory exists
+os_source_image_artifact_dir = File.dirname(os_source_image_artifact)
+FileUtils.mkdir_p(os_source_image_artifact_dir)
+
+os_source_images.split(OS_SOURCE_IMAGE_SEPARATOR).each do |image|
+    std_out, std_err, status = Open3.capture3("openstack image show -c id -f value '#{image}'")
+    if status.exitstatus != 0
+        unless std_err.include?(RESOURCE_NOT_FOUND_ERROR)
+            raise("Error connecting to OpenStack: #{std_err}")
+        end
+    else
+        image_id = std_out.strip!
+        File.write(os_source_image_artifact, image_id)
+        puts("Written source image ID \"#{image_id}\" to \"#{os_source_image_artifact}\"")
+        break
+    end
+end
