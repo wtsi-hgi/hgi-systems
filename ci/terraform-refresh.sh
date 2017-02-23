09a85d261f3997f9aba76ea6ada5ab5687b19766
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-02-23 10:44:50 +0000

    improve error handling in terraform-refresh

--- ci/terraform-refresh.sh
+++ ci/terraform-refresh.sh
@@ -13,11 +13,11 @@ if [[ -e "${ENV}.tfstate" ]]; then
     echo "Calling terraform refresh on ${ENV}.tfstate"
     terraform refresh -state="${ENV}.tfstate" -backup=-
     terraform_exit_status=$?
-    cp "terraform/${REGION}/${ENV}.tfstate" "${CI_PROJECT_DIR}/artifacts/"
+    cp "${ENV}.tfstate" "${CI_PROJECT_DIR}/artifacts/" || echo "WARNING: ${ENV}.tfstate not present after refresh"
     if [[ ${terraform_exit_status} -eq 0 ]]; then
         echo "Terraform refresh was successful, generating human readable ${ENV}.tfstate.txt artifact"
         (terraform show -no-color "${ENV}.tfstate" > "${ENV}.tfstate.txt")
-        cp "terraform/${REGION}/${ENV}.tfstate.txt" "${CI_PROJECT_DIR}/artifacts/"
+        cp "${ENV}.tfstate.txt" "${CI_PROJECT_DIR}/artifacts/"
     else
         >&2 echo "Terraform refresh failed: ${terraform_exit_status}"
         exit ${terraform_exit_status}
