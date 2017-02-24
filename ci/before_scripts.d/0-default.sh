d3c3313cd8a12d7af2133d9c7f21adba776cda7a
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-02-24 15:44:01 +0000

    try changing s3 region to cog as it may not matter

--- ci/before_scripts.d/0-default.sh
+++ ci/before_scripts.d/0-default.sh
@@ -1,4 +1,4 @@
 export VERSION_ABBREV_LEN="8"
 export VERSION_COMMAND="git describe --tags --dirty --always --abbrev=${VERSION_ABBREV_LEN}"
 
-export TERRAFORM_S3_REGION=""
+export TERRAFORM_S3_REGION="cog"
