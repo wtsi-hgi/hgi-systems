3e8bdfec87a914fe273d5296fe0d867ae3bffefe
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-02-24 15:49:34 +0000

    add checks on terragrunt AWS vars in ci/terraform-validate.sh

--- ci/terraform-validate.sh
+++ ci/terraform-validate.sh
@@ -2,6 +2,16 @@
 
 set -euf -o pipefail
 
+if [[ -z "${AWS_ACCESS_KEY_ID:-}" ]]; then
+    >&2 echo "AWS_ACCESS_KEY_ID not set, giving up on terragrunt"
+    exit 1
+fi
+
+if [[ -z "${AWS_SECRET_KEY_ID:-}" ]]; then
+    >&2 echo "AWS_SECRET_KEY_ID not set, giving up on terragrunt"
+    exit 1
+fi
+
 if [[ -d "terraform/${REGION}" ]]; then
     echo "Calling terragrunt validate in terraform/${REGION}"
     cd terraform/${REGION} && terragrunt validate
