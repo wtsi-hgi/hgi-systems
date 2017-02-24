2f65c4670fd4f5200375b3c9afd5ccd5425988b0
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-02-24 15:19:25 +0000

    change terragrunt variable names to match what is set in build variables

--- ci/before_scripts.d/60-terragrunt-locks-aws-credentials.sh
+++ ci/before_scripts.d/60-terragrunt-locks-aws-credentials.sh
@@ -3,17 +3,17 @@ set -euf -o pipefail
 
 # TODO could limit this to terraform jobs
 
-if [[ -n "${TERRAGRUNT_LOCKS_AWS_ACCESS_KEY_ID}" ]]; then
-    export AWS_ACCESS_KEY_ID=${TERRAGRUNT_LOCKS_AWS_ACCESS_KEY_ID}
+if [[ -n "${TERRAGRUNT_LOCKS_AWS_ACCESS_KEY}" ]]; then
+    export AWS_ACCESS_KEY_ID=${TERRAGRUNT_LOCKS_AWS_ACCESS_KEY}
 else
-    >&2 echo "TERRAGRUNT_LOCKS_AWS_ACCESS_KEY_ID was unset or empty"
+    >&2 echo "TERRAGRUNT_LOCKS_AWS_ACCESS_KEY was unset or empty"
     exit 1
 fi
 
-if [[ -n "${TERRAGRUNT_LOCKS_AWS_SECRET_KEY_ID}" ]]; then
-    export AWS_SECRET_KEY_ID=${TERRAGRUNT_LOCKS_AWS_SECRET_KEY_ID}
+if [[ -n "${TERRAGRUNT_LOCKS_AWS_SECRET_KEY}" ]]; then
+    export AWS_SECRET_KEY_ID=${TERRAGRUNT_LOCKS_AWS_SECRET_KEY}
 else
-    >&2 echo "TERRAGRUNT_LOCKS_AWS_SECRET_KEY_ID was unset or empty"
+    >&2 echo "TERRAGRUNT_LOCKS_AWS_SECRET_KEY was unset or empty"
     exit 1
 fi
 
