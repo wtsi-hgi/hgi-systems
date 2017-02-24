6cdb0271deefb203d6e7f53a29121e97840b03c4
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-02-24 15:46:43 +0000

    add logging to terragrunt locks before script

--- ci/before_scripts.d/60-terragrunt-locks-aws-credentials.sh
+++ ci/before_scripts.d/60-terragrunt-locks-aws-credentials.sh
@@ -4,6 +4,7 @@ set -euf -o pipefail
 # TODO could limit this to terraform jobs
 
 if [[ -n "${TERRAGRUNT_LOCKS_AWS_ACCESS_KEY}" ]]; then
+    echo "Setting AWS_ACCESS_KEY_ID"
     export AWS_ACCESS_KEY_ID=${TERRAGRUNT_LOCKS_AWS_ACCESS_KEY}
 else
     >&2 echo "TERRAGRUNT_LOCKS_AWS_ACCESS_KEY was unset or empty"
@@ -11,6 +12,7 @@ else
 fi
 
 if [[ -n "${TERRAGRUNT_LOCKS_AWS_SECRET_KEY}" ]]; then
+    echo "Setting AWS_SECRET_KEY_ID"
     export AWS_SECRET_KEY_ID=${TERRAGRUNT_LOCKS_AWS_SECRET_KEY}
 else
     >&2 echo "TERRAGRUNT_LOCKS_AWS_SECRET_KEY was unset or empty"
