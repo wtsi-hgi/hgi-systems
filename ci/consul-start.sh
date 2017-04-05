b764ad77c0256c316b62d3ef66e6dbf4b9333e5b
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-04-05 14:24:27 +0100

    Moves responsiblity of installing consul into the Docker run container.

--- ci/consul-start.sh
+++ ci/consul-start.sh
@@ -3,26 +3,15 @@
 set -euf -o pipefail
 
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${SCRIPT_DIRECTORY}/common.sh"
 
 # Settings
-CONSUL_INSTALL_DIRECTORY=/tmp/consul/bin
 CONSUL_DATA_DIRECTORY=/tmp/consul/data
-CONSUL_DOWNLOAD_SOURCE=https://releases.hashicorp.com/consul/0.7.5/consul_0.7.5_linux_amd64.zip
-CONSUL_DOWNLOAD_DESTINATION=/tmp/consul.zip
-COMMON_SCRIPT="${SCRIPT_DIRECTORY}/common.sh"
 
 # Ensure required variables are set
-source "${COMMON_SCRIPT}"
 ensure_set CONSUL_DATACENTER CONSUL_JOIN_ADDRESS CONSUL_ENCRYPT_KEY
 
-# Install consul
-mkdir -p "${CONSUL_INSTALL_DIRECTORY}"
-wget "${CONSUL_DOWNLOAD_SOURCE}" -O "${CONSUL_DOWNLOAD_DESTINATION}"
-unzip -d "${CONSUL_INSTALL_DIRECTORY}" "${CONSUL_DOWNLOAD_DESTINATION}"
-test -f "${CONSUL_INSTALL_DIRECTORY}/consul"
-rm "${CONSUL_DOWNLOAD_DESTINATION}"
-export PATH="${CONSUL_INSTALL_DIRECTORY}:${PATH}"
-
 # Setup consul agent
+rm -rf "${CONSUL_DATA_DIRECTORY}"
 mkdir -p "${CONSUL_DATA_DIRECTORY}"
 consul agent -data-dir "${CONSUL_DATA_DIRECTORY}" -datacenter="${CONSUL_DATACENTER}" -join "${CONSUL_JOIN_ADDRESS}" -encrypt="${CONSUL_ENCRYPT_KEY}"
