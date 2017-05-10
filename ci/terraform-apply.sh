8d560dd13d41f8166eefe9fa30d990a60479fe2d
Mercury <mercury@sanger.ac.uk>
2017-05-10 15:14:02 +0100

    merge plan and apply stages

--- ci/terraform-apply.sh
+++ ci/terraform-apply.sh
@@ -3,28 +3,43 @@
 set -euf -o pipefail
 
 artifacts_dir="${CI_PROJECT_DIR}/artifacts"
+mkdir -p "${artifacts_dir}"
 echo "Listing contents of artifacts directory ${artifacts_dir}"
 artifacts=$(ls "${artifacts_dir}/")
 
-if [[ -n "${artifacts}" ]]; then
-    for artifact in ${artifacts}; do
-        echo "Copying ${artifacts_dir}/${artifact} to terraform/${REGION}/"
-        cp "${artifacts_dir}/${artifact}" "terraform/${REGION}/"
-    done
-else
-    echo "No artifacts to copy"
-fi
-
-echo "Emptying artifacts directory"
-rm -rf "${CI_PROJECT_DIR}/artifacts" && mkdir -p "${CI_PROJECT_DIR}/artifacts"
-
-echo "Generating /tmp/ansible_vault.pw"
-(echo "$ANSIBLE_VAULT_PASSWORD" > /tmp/ansible_vault.pw)
-
-cd "terraform/${REGION}"
+echo "Changing to terraform/${REGION} directory"
+cd terraform/${REGION}
 
 echo "Calling terraform init"
 terraform init
+
+echo "Calling terraform plan"
+terraform plan -input=false -out plan
+plan_exit_status=$?
+echo "Copying plan to artifacts"
+cp plan "${artifacts_dir}/"
+
+if [[ ${plan_exit_status} -eq 0 ]]; then
+    echo "Terraform plan was successful"
+    echo "Generating human readable ${ENV}.tfstate.txt artifact"
+    (terraform show -no-color > "${ENV}.tfstate.txt")
+    cp "${ENV}.tfstate.txt" "${artifacts_dir}"
+    echo "Generating human-readable plan.txt artifact"
+    terraform show -no-color plan > plan.txt
+    cp plan.txt "${artifacts_dir}/"
+    echo "Generating dot graph plan.dot from plan"
+    terraform graph plan > plan.dot
+    cp plan.dot "${artifacts_dir}/"
+    echo "Generating plan.png PNG from graph"
+    dot -Tpng < plan.dot > plan.png
+    cp plan.png "${artifacts_dir}/" 
+else
+    >&2 echo "Terraform plan failed: ${plan_exit_status}"
+    exit ${plan_exit_status}
+fi
+
+echo "Generating /tmp/ansible_vault.pw"
+(echo "$ANSIBLE_VAULT_PASSWORD" > /tmp/ansible_vault.pw)
 
 echo "Calling terraform apply"
 set +e
