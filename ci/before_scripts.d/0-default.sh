faae18a258f23e411f401a3fdfd6cd44fbe716bc
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-05-11 16:20:21 +0000

    disable TF_LOG

--- ci/before_scripts.d/0-default.sh
+++ ci/before_scripts.d/0-default.sh
@@ -2,4 +2,4 @@ export VERSION_ABBREV_LEN="8"
 export VERSION_COMMAND="git describe --tags --dirty --always --abbrev=${VERSION_ABBREV_LEN}"
 
 export TERRAFORM_S3_REGION="cog"
-export TF_LOG="WARN"
+#export TF_LOG="WARN"
