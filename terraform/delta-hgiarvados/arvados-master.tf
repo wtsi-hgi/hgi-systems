92a185e3f8296d9b4df00041736e1f72f075131f
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-05-18 10:46:18 +0000

    add arvados-master to delta-hgiarvados terraform

---
+++ terraform/delta-hgiarvados/arvados-master.tf
@@ -0,0 +1,35 @@
+resource "openstack_compute_instance_v2" "arvados-master-delta-hgiarvados" {
+  provider = "openstack.delta-hgiarvados"
+  count = 1
+  name = "arvados-master-delta-hgiarvados"
+  image_name = "${var.base_image_name}"
+  flavor_name = "m1.xlarge"
+  key_pair = "${openstack_compute_keypair_v2.mercury_delta-hgiarvados.id}"
+  security_groups = ["${openstack_compute_secgroup_v2.ssh_delta-hgiarvados.id}"]
+  network {
+    uuid = "${openstack_networking_network_v2.main_delta-hgiarvados.id}"
+    access_network = true
+  }
+
+  metadata = {
+    ansible_groups = "arvados_masters"
+  }
+
+  # wait for host to be available via ssh
+  provisioner "remote-exec" {
+    inline = [
+      "hostname"
+    ]
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      agent = "true"
+      timeout = "2m"
+    }
+  }
+}
+
+output "arvados_master_delta-hgiarvados_ip" {
+  value = "${openstack_compute_instance_v2.arvados-master-delta-hgiarvados.access_ip_v4}"
+}
+
