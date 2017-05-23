b04155cbfee6eff4bd1344f53a7b0de14c3b3c51
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-05-23 16:46:02 +0100

    refer to image name and user by var name prefix rather than map

--- terraform/delta-hgiarvados/images.tf
+++ terraform/delta-hgiarvados/images.tf
@@ -1,32 +1,8 @@
-variable base_image {
-  type = "map"
-  default = {
-    name = "${var.base_image_name}"
-    user = "${var.base_image_user}"
-  }
-}
-
-variable debian_base_image {
-  type = "map"
-  default = {
-    name = "${var.debian_base_image_name}"
-    user = "${var.debian_base_image_user}"
-  }
-}
-
-variable docker_image {
-  type = "map"
-  default = {
-    name = "${var.docker_image_name}"
-    user = "${var.docker_image_user}"
-  }
-}
-
 # N.B. this output helps to keep terraform working even if no instances exist to produce output
 output "image_names" {
   value = {
-    "base" = "${var.base_image["name"]}"
-    "debian_base" = "${var.debian_base_image["name"]}"
-    "docker" = "${var.docker_image["name"]}"
+    "base" = "${var.base_image_name}"
+    "debian_base" = "${var.debian_base_image_name}"
+    "docker" = "${var.docker_image_name}"
   }
 }
--- terraform/delta-hgiarvados/ssh-gateway.tf
+++ terraform/delta-hgiarvados/ssh-gateway.tf
@@ -7,7 +7,7 @@ resource "openstack_compute_instance_v2" "ssh-gateway-delta-hgiarvados" {
   provider = "openstack.delta-hgiarvados"
   count = 1
   name = "ssh-gateway-delta-hgiarvados"
-  image_name = "${var.base_image["name"]}"
+  image_name = "${var.base_image_name}"
   flavor_name = "m1.small"
   key_pair = "${openstack_compute_keypair_v2.mercury_delta-hgiarvados.id}"
   security_groups = ["${openstack_compute_secgroup_v2.ssh_delta-hgiarvados.id}"]
@@ -28,7 +28,7 @@ resource "openstack_compute_instance_v2" "ssh-gateway-delta-hgiarvados" {
     ]
     connection {
       type = "ssh"
-      user = "${var.base_image["user"]}"
+      user = "${var.base_image_user}"
       agent = "true"
       timeout = "2m"
     }
