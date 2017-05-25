5eff6aade842baba6ac71ddf6a32aa49279dfac7
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-05-25 16:30:31 +0100

    Enforce "OS_TENANT" is set for Terraform.

--- ci/before_scripts.d/20-os-vars.sh
+++ ci/before_scripts.d/20-os-vars.sh
@@ -5,16 +5,19 @@ elif [[ "$REGION" == "delta-hgi" ]]; then
     export OS_USERNAME=${DELTA_OS_USERNAME}
     export OS_PASSWORD=${DELTA_OS_PASSWORD}
     export OS_AUTH_URL=${DELTA_OS_AUTH_URL}
+    export OS_TENANT_NAME="hgi"
     echo "OS vars for delta-hgi set"
 elif [[ "$REGION" == "delta-hgiarvados" ]]; then 
     export OS_USERNAME=${DELTA_OS_USERNAME}
     export OS_PASSWORD=${DELTA_OS_PASSWORD}
     export OS_AUTH_URL=${DELTA_OS_AUTH_URL}
+    export OS_TENANT_NAME="hgiarvados"
     echo "OS vars for delta-hgiarvados set"
 elif [[ "$REGION" == "emedlab-arvados" ]]; then 
     export OS_USERNAME=${EMEDLAB_OS_USERNAME}
     export OS_PASSWORD=${EMEDLAB_OS_PASSWORD}
     export OS_AUTH_URL=${EMEDLAB_OS_AUTH_URL}
+    export OS_TENANT_NAME="arvados"
     export HTTP_PROXY=${EMEDLAB_HTTP_PROXY}
     export HTTPS_PROXY=${EMEDLAB_HTTP_PROXY}
     echo "OS vars for emedlab-arvados set"
