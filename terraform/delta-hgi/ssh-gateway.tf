8f42cd8461d081b50e3c0c84acf75d5f21922c1e
Mercury <mercury@sanger.ac.uk>
2017-05-30 17:11:13 +0100

    debug 4

--- terraform/delta-hgi/ssh-gateway.tf
+++ terraform/delta-hgi/ssh-gateway.tf
@@ -6,7 +6,7 @@ module "ssh-gateway" {
   }
   flavour = "m1.small"
   domain = "delta-hgi.hgi.sanger.ac.uk"
-  security_groups = ["${openstack_compute_secgroup_v2.ssh_delta-hgi.id}"]
+  security_groups123 = ["${openstack_compute_secgroup_v2.ssh_delta-hgi.id}"]
   key_pair_id = "${openstack_compute_keypair_v2.mercury_delta-hgi.id}"
   network_id = "${openstack_networking_network_v2.main_delta-hgi.id}"
 }
