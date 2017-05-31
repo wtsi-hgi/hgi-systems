f5a7bd5cb34175977b2368ccd2f46363e8693952
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-05-31 16:10:05 +0100

    name openstack module resources based on region/env vars

--- terraform/modules/openstack/main.tf
+++ terraform/modules/openstack/main.tf
@@ -1,35 +1,36 @@
 variable "env" {}
+varaible "region" {}
 variable "mercury_keypair" {}
 variable "jr17_keypair" {}
 
 ###############################################################################
 # Key Pairs
 ###############################################################################
-resource "openstack_compute_keypair_v2" "mercury_delta-hgiarvados" {
+resource "openstack_compute_keypair_v2" "mercury_${var.region}_${var.env}" {
   provider = "openstack"
-  name = "mercury_delta-hgiarvados"
+  name = "mercury_${var.region}_${var.env}"
   public_key = "${var.mercury_keypair}"
 }
 
-resource "openstack_compute_keypair_v2" "jr17_delta-hgiarvados" {
+resource "openstack_compute_keypair_v2" "jr17_${var.region}_${var.env}" {
   provider = "openstack"
-  name = "jr17_delta-hgiarvados"
+  name = "jr17_${var.region}_${var.env}"
   public_key = "${var.jr17_keypair}"
 }
 
 output "key_pair_ids" {
   value = { 
-    "mercury" = "${openstack_compute_keypair_v2.mercury_delta-hgiarvados.id}"
-    "jr17" = "${openstack_compute_keypair_v2.jr17_delta-hgiarvados.id}"
+    "mercury" = "${openstack_compute_keypair_v2.mercury_${var.region}_${var.env}.id}"
+    "jr17" = "${openstack_compute_keypair_v2.jr17_${var.region}_${var.env}.id}"
   }
 }
 
 ###############################################################################
 # Security Groups
 ###############################################################################
-resource "openstack_compute_secgroup_v2" "ssh_delta-hgiarvados" {
+resource "openstack_compute_secgroup_v2" "ssh_${var.region}_${var.env}" {
   provider = "openstack"
-  name = "ssh_delta-hgiarvados"
+  name = "ssh_${var.region}_${var.env}"
   description = "Incoming ssh access"
   rule {
     from_port = 22
@@ -41,7 +42,7 @@ resource "openstack_compute_secgroup_v2" "ssh_delta-hgiarvados" {
 
 output "security_group_ids" {
   value = {
-    "ssh" = "${openstack_compute_secgroup_v2.ssh_delta-hgiarvados.id}"
+    "ssh" = "${openstack_compute_secgroup_v2.ssh_${var.region}_${var.env}.id}"
   }
   
 }
@@ -49,33 +50,33 @@ output "security_group_ids" {
 ###############################################################################
 # Networks, Subnets, Routers
 ###############################################################################
-resource "openstack_networking_network_v2" "main_delta-hgiarvados" {
+resource "openstack_networking_network_v2" "main_${var.region}_${var.env}" {
   provider = "openstack"
-  name = "main_delta-hgiarvados"
+  name = "main_${var.region}_${var.env}"
   admin_state_up = "true"
 }
 
 output "network_id" {
-  value = "${openstack_networking_network_v2.main_delta-hgiarvados.id}"
+  value = "${openstack_networking_network_v2.main_${var.region}_${var.env}.id}"
 }
 
-resource "openstack_networking_subnet_v2" "main_delta-hgiarvados" {
+resource "openstack_networking_subnet_v2" "main_${var.region}_${var.env}" {
   provider = "openstack"
-  name = "main_delta-hgiarvados"
-  network_id = "${openstack_networking_network_v2.main_delta-hgiarvados.id}"
+  name = "main_${var.region}_${var.env}"
+  network_id = "${openstack_networking_network_v2.main_${var.region}_${var.env}.id}"
   cidr = "10.101.0.0/24"
   ip_version = 4
   dns_nameservers = ["172.18.255.1", "172.18.255.2", "172.18.255.3"]
 }
 
-resource "openstack_networking_router_v2" "main_nova_delta-hgiarvados" {
+resource "openstack_networking_router_v2" "main_nova_${var.region}_${var.env}" {
   provider = "openstack"
-  name = "main_nova_delta-hgiarvados"
+  name = "main_nova_${var.region}_${var.env}"
   external_gateway = "9f50f282-2a4c-47da-88f8-c77b6655c7db"
 }
 
-resource "openstack_networking_router_interface_v2" "main_nova_delta-hgiarvados" {
+resource "openstack_networking_router_interface_v2" "main_nova_${var.region}_${var.env}" {
   provider = "openstack"
-  router_id = "${openstack_networking_router_v2.main_nova_delta-hgiarvados.id}"
-  subnet_id = "${openstack_networking_subnet_v2.main_delta-hgiarvados.id}"
+  router_id = "${openstack_networking_router_v2.main_nova_${var.region}_${var.env}.id}"
+  subnet_id = "${openstack_networking_subnet_v2.main_${var.region}_${var.env}.id}"
 }
