273a3d35ef79487094c78b07209f1f818124eddc
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-06-02 13:48:12 +0100

    add pre-commit hook

--- terraform/modules/arvados-sso/main.tf
+++ terraform/modules/arvados-sso/main.tf
@@ -1,65 +1,18 @@
-variable "flavour" {}
-variable "domain" {}
-variable "network_id" {}
+module "arvados-sso" {
+  source = "../modules/arvados-sso"
 
-variable "security_group_ids" {
-  type    = "map"
-  default = {}
-}
-
-variable "key_pair_ids" {
-  type    = "map"
-  default = {}
-}
-
-variable "image" {
-  type    = "map"
-  default = {}
-}
-
-variable "bastion" {
-  type    = "map"
-  default = {}
-}
-
-resource "openstack_compute_instance_v2" "arvados-sso" {
-  provider        = "openstack"
-  count           = 1
-  name            = "arvados-sso"
-  image_name      = "${var.image["name"]}"
-  flavor_name     = "${var.flavour}"
-  key_pair        = "${var.key_pair_ids["mercury"]}"
-  security_groups = ["${var.security_group_ids["ssh"]}"]
-
-  network {
-    uuid           = "${var.network_id}"
-    access_network = true
+  image = {
+    name = "${var.base_image_name}"
+    user = "${var.base_image_user}"
   }
 
-  metadata = {
-    ansible_groups = "arvados-ssos,arvados-cluster-ncucu"
-    user           = "${var.image["user"]}"
-    bastion_host   = "${var.bastion["host"]}"
-    bastion_user   = "${var.bastion["user"]}"
+  flavour            = "m1.medium"
+  domain             = "delta-hgiarvados.hgi.sanger.ac.uk"
+  security_group_ids = "${module.openstack.security_group_ids}"
+  key_pair_ids       = "${module.openstack.key_pair_ids}"
+  network_id         = "${module.openstack.network_id}"
+  bastion	     = {
+    host = "${module.ssh-gateway.host}"
+    user = "${module.ssh-gateway.user}"
   }
-
-  # wait for host to be available via ssh
-  provisioner "remote-exec" {
-    inline = [
-      "hostname",
-    ]
-
-    connection {
-      type         = "ssh"
-      user         = "${var.image["user"]}"
-      agent        = "true"
-      timeout      = "2m"
-      bastion_host = "${var.bastion["host"]}"
-      bastion_user = "${var.bastion["user"]}"
-    }
-  }
-}
-
-output "ip" {
-  value = "${openstack_compute_instance_v2.arvados-sso.access_ip_v4}"
 }
