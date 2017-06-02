c31019f7f1fee4e705d9f952ddf6722941e07154
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-06-02 16:50:34 +0100

    update output from terraform-fmt.sh

--- ci/terraform-fmt.sh
+++ ci/terraform-fmt.sh
@@ -17,11 +17,11 @@ fi
 >&2 echo -n "Calling terraform fmt in ${dir} directory... "
 fmt_diff=$(cd ${dir} && ${terraform_bin} fmt -write=false -diff=true)
 if [[ -n "${fmt_diff}" ]]; then
-  >&2 echo "error!"
-  >&2 echo -e '\e[31mERROR: terraform fmt indicates formatting changes are required, suggest using a pre-commit hook to run `terraform fmt`'
-  >&2 echo -e "\e[91m${fmt_diff}"
+  >&2 echo -e "\e[31merror!\e[0m"
+  >&2 echo -e '\e[31mterraform fmt indicates formatting changes are required, please run `terraform fmt` or make the following changes manually:\e[0m'
+  >&2 echo -e "\e[91m${fmt_diff}\e[0m"
   exit 1
 fi
 
->&2 echo "ok."
+>&2 echo -e "\e[32mok.\e[0m"
 exit 0
