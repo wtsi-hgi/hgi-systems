a42f7f677c6b08c61d400408008e04574f8956a9
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-06-24 22:54:02 +0100

    add passenger and arvados-sso installation tasks

--- terraform/modules/arvados-sso/main.tf
+++ terraform/modules/arvados-sso/main.tf
@@ -61,6 +61,14 @@ resource "openstack_compute_instance_v2" "arvados-sso" {
   }
 }
 
+resource "infoblox_record" "ssh-gateway" {
+  value  = "${openstack_compute_instance_v2.arvados-sso.access_ip_v4}"
+  name   = "arvados-sso-${var.arvados_cluster_id}"
+  domain = "${var.domain}"
+  type   = "A"
+  ttl    = 600
+}
+
 output "ip" {
   value = "${openstack_compute_instance_v2.arvados-sso.access_ip_v4}"
 }
