443e0d0d20c5489e347e7838f6de090932f6d254
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-07-26 12:42:56 +0100

    add spark master and compute

--- terraform/delta-hgi/spark-01.tf
+++ terraform/delta-hgi/spark-01.tf
@@ -19,3 +19,26 @@ module "spark-master" {
 
   spark_cluster_id = "01"
 }
+
+module "spark-compute" {
+  count  = 1
+  source = "../modules/spark-compute"
+
+  image = {
+    name = "${var.base_image_name}"
+    user = "${var.base_image_user}"
+  }
+
+  flavour            = "m1.large"
+  domain             = "hgi.sanger.ac.uk"
+  security_group_ids = "${module.openstack.security_group_ids}"
+  key_pair_ids       = "${module.openstack.key_pair_ids}"
+  network_id         = "${module.openstack.network_id}"
+
+  bastion = {
+    host = "${module.ssh-gateway.host}"
+    user = "${module.ssh-gateway.user}"
+  }
+
+  spark_cluster_id = "01"
+}
---
+++ terraform/modules/spark-compute/main.tf
@@ -0,0 +1,72 @@
+variable "flavour" {}
+variable "domain" {}
+variable "network_id" {}
+variable "spark_cluster_id" {}
+
+variable "security_group_ids" {
+  type    = "map"
+  default = {}
+}
+
+variable "key_pair_ids" {
+  type    = "map"
+  default = {}
+}
+
+variable "image" {
+  type    = "map"
+  default = {}
+}
+
+variable "bastion" {
+  type    = "map"
+  default = {}
+}
+
+resource "openstack_compute_floatingip_v2" "spark-compute" {
+  provider = "openstack"
+  pool     = "nova"
+}
+
+resource "openstack_compute_instance_v2" "spark-compute" {
+  provider        = "openstack"
+  count           = 1
+  name            = "spark-compute"
+  image_name      = "${var.image["name"]}"
+  flavor_name     = "${var.flavour}"
+  key_pair        = "${var.key_pair_ids["mercury"]}"
+  security_groups = ["${var.security_group_ids["ssh"]}", "${var.security_group_ids["https"]}", "${var.security_group_ids["tcp-local"]}", "${var.security_group_ids["udp-local"]}"]
+
+  network {
+    uuid           = "${var.network_id}"
+    floating_ip    = "${openstack_compute_floatingip_v2.spark-compute.address}"
+    access_network = true
+  }
+
+  metadata = {
+    ansible_groups = "spark-computers spark-cluster-${var.spark_cluster_id}-members hgi-credentials"
+    user           = "${var.image["user"]}"
+    bastion_host   = "${var.bastion["host"]}"
+    bastion_user   = "${var.bastion["user"]}"
+  }
+
+  # wait for host to be available via ssh
+  provisioner "remote-exec" {
+    inline = [
+      "hostname",
+    ]
+
+    connection {
+      type         = "ssh"
+      user         = "${var.image["user"]}"
+      agent        = "true"
+      timeout      = "2m"
+      bastion_host = "${var.bastion["host"]}"
+      bastion_user = "${var.bastion["user"]}"
+    }
+  }
+}
+
+output "ip" {
+  value = "${openstack_compute_instance_v2.spark-compute.access_ip_v4}"
+}
