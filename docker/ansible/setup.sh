69ea2bb048d0092f9d2515fbc6cd811d62ad04e8
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-08-03 18:08:58 +0100

    Makes Docker use Python requirements files.

--- docker/ansible/setup.sh
+++ docker/ansible/setup.sh
@@ -10,7 +10,6 @@ if [ -z ${!ssh_key_location_parameter} ]; then
 elif [ -z ${!vault_location_parameter} ]; then
     unsetParameter=$vault_location_parameter
 fi
-
 if [ "${unsetParameter}" ]; then
     >&2 echo "${unsetParameter} is not set"
     exit 1
@@ -21,8 +20,14 @@ set -u
 SSH_KEY_LOCATION=${!ssh_key_location_parameter}
 VAULT_LOCATION=${!vault_location_parameter}
 
+>&2 echo "Creating symlinks for SSH key and vault password..."
 ln -s "${SSH_KEY_LOCATION}" ~/.ssh/id_rsa
 ln -s "${SSH_KEY_LOCATION}" ~mercury/.ssh/id_rsa
 ln -s "${VAULT_LOCATION}" ~/vault.pw
 
+>&2 echo "Ensuring Python requirements are up to date..."
+pip2 install -q -r /hgi-systems/ansible/py2-requirements.txt
+pip3 install -q -r /hgi-systems/ansible/py3-requirements.txt
+
+>&2 echo "Ready!"
 bash -c "$*"
