7ba99f0f56279443ec56ce7c934f552b81a0d254
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-09-15 11:34:28 +0100

    change remote state consul url to updated hostnames

--- terraform/delta-hgi/remote-state.tf
+++ terraform/delta-hgi/remote-state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "consul" {
-    address = "consul-delta.hgi.sanger.ac.uk:8500"
+    address = "consul-delta-hgi.hgi.sanger.ac.uk:8500"
     path    = "terraform/delta-hgi"
   }
 }
