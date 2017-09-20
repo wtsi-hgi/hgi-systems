fe586eba2fc90b279e6df0e284be9c1b94f040e6
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-09-20 13:15:14 +0100

    add ssh gateways to consul-clients security groups

--- terraform/modules/ssh-gateway/main.tf
+++ terraform/modules/ssh-gateway/main.tf
@@ -35,13 +35,17 @@ resource "openstack_networking_floatingip_v2" "ssh-gateway" {
 }
 
 resource "openstack_compute_instance_v2" "ssh-gateway" {
-  provider        = "openstack"
-  count           = 1
-  name            = "ssh-gateway"
-  image_name      = "${var.image["name"]}"
-  flavor_name     = "${var.flavour}"
-  key_pair        = "${var.key_pair_ids["mercury"]}"
-  security_groups = ["${var.security_group_ids["ssh"]}"]
+  provider    = "openstack"
+  count       = 1
+  name        = "ssh-gateway"
+  image_name  = "${var.image["name"]}"
+  flavor_name = "${var.flavour}"
+  key_pair    = "${var.key_pair_ids["mercury"]}"
+
+  security_groups = [
+    "${var.security_group_ids["ssh"]}",
+    "${var.security_group_ids["consul-client"]}",
+  ]
 
   network {
     uuid           = "${var.network_id}"
