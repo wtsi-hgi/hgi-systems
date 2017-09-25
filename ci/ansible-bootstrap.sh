99d75c5f9a3a68fff158af5e604535e89916a3d2
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-09-25 16:58:56 +0100

    add ci/ansible-bootstrap.sh

---
+++ ci/ansible-bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${SCRIPT_DIRECTORY}/common.sh"
+
+ensureSet CI_PROJECT_DIR REGION ENV ANSIBLE_VAULT_PASSWORD_FILE
+
+echo "Changing to ansible directory"
+cd ansible
+
+export ANSIBLE_CONFIG="${CI_PROJECT_DIR}/ansible/ansible.cfg"
+inventory=bootstrap_hosts.d
+echo "Calling ansible-playbook bootstrap.yml on inventory ${inventory}"
+ansible-playbook -i ${inventory} --vault-password-file "${ANSIBLE_VAULT_PASSWORD_FILE}" bootstrap.yml
+playbook_exit_status=$?
+
+if [[ ${playbook_exit_status} -eq 0 ]]; then
+    echo "ansible-playbook was successful"
+else
+    >&2 echo "ansible-playbook failed: ${playbook_exit_status}"
+    exit ${playbook_exit_status}
+fi
+
