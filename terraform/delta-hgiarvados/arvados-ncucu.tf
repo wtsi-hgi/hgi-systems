2085d9fc760d4ae83996977efead661e540a8ea5
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-10-02 22:50:55 +0100

    add arvados-shell terraform module and ansible role

--- terraform/delta-hgiarvados/arvados-ncucu.tf
+++ terraform/delta-hgiarvados/arvados-ncucu.tf
@@ -114,6 +114,29 @@ module "arvados-keep" {
   extra_ansible_groups = ["consul-cluster-delta-hgiarvados"]
 }
 
+module "arvados-shell" {
+  source = "../modules/arvados-shell"
+
+  image = {
+    name = "${var.base_image_name}"
+    user = "${var.base_image_user}"
+  }
+
+  flavour            = "m1.small"
+  domain             = "hgi.sanger.ac.uk"
+  security_group_ids = "${module.openstack.security_group_ids}"
+  key_pair_ids       = "${module.openstack.key_pair_ids}"
+  network_id         = "${module.openstack.network_id}"
+
+  bastion = {
+    host = "${module.ssh-gateway.host}"
+    user = "${module.ssh-gateway.user}"
+  }
+
+  arvados_cluster_id   = "ncucu"
+  extra_ansible_groups = ["consul-cluster-delta-hgiarvados"]
+}
+
 module "arvados-compute-node" {
   source = "../modules/arvados-compute-node"
 
