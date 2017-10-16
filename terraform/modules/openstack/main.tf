4ad804d70f8576222c41a011422d3dec31af7001
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-10-16 11:24:29 +0100

    add missing keep-proxy entry in security_group_ids map

--- terraform/modules/openstack/main.tf
+++ terraform/modules/openstack/main.tf
@@ -266,6 +266,7 @@ output "security_group_ids" {
     slurm-master  = "${openstack_compute_secgroup_v2.slurm-master.id}"
     slurm-compute = "${openstack_compute_secgroup_v2.slurm-compute.id}"
     keep-service  = "${openstack_compute_secgroup_v2.keep-service.id}"
+    keep-proxy    = "${openstack_compute_secgroup_v2.keep-proxy.id}"
   }
 }
 
