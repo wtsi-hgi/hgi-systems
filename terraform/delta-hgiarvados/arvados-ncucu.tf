adf3e301e4cfe8a4bd9a7e07208252232f792eb2
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-10-16 12:36:59 +0100

    Revert "remove entire arvados-ncucu cluster from terraform"
    This reverts commit 65936757010de4650ea2a7d135e8a7fca46273c4.

---
+++ terraform/delta-hgiarvados/arvados-ncucu.tf
@@ -0,0 +1,162 @@
+module "arvados-master" {
+  source = "../modules/arvados-master"
+
+  image = {
+    name = "${var.base_image_name}"
+    user = "${var.base_image_user}"
+  }
+
+  flavour            = "m1.xlarge"
+  domain             = "hgi.sanger.ac.uk"
+  security_group_ids = "${module.openstack.security_group_ids}"
+  key_pair_ids       = "${module.openstack.key_pair_ids}"
+  network_id         = "${module.openstack.network_id}"
+
+  bastion = {
+    host = "${module.ssh-gateway.host}"
+    user = "${module.ssh-gateway.user}"
+  }
+
+  arvados_cluster_id   = "ncucu"
+  extra_ansible_groups = ["consul-cluster-delta-hgiarvados"]
+}
+
+module "arvados-sso" {
+  source = "../modules/arvados-sso"
+
+  image = {
+    name = "${var.base_image_name}"
+    user = "${var.base_image_user}"
+  }
+
+  flavour            = "m1.medium"
+  domain             = "hgi.sanger.ac.uk"
+  security_group_ids = "${module.openstack.security_group_ids}"
+  key_pair_ids       = "${module.openstack.key_pair_ids}"
+  network_id         = "${module.openstack.network_id}"
+
+  bastion = {
+    host = "${module.ssh-gateway.host}"
+    user = "${module.ssh-gateway.user}"
+  }
+
+  arvados_cluster_id   = "ncucu"
+  extra_ansible_groups = ["consul-cluster-delta-hgiarvados"]
+}
+
+module "arvados-workbench" {
+  source = "../modules/arvados-workbench"
+
+  image = {
+    name = "${var.base_image_name}"
+    user = "${var.base_image_user}"
+  }
+
+  flavour            = "m1.medium"
+  domain             = "hgi.sanger.ac.uk"
+  security_group_ids = "${module.openstack.security_group_ids}"
+  key_pair_ids       = "${module.openstack.key_pair_ids}"
+  network_id         = "${module.openstack.network_id}"
+
+  bastion = {
+    host = "${module.ssh-gateway.host}"
+    user = "${module.ssh-gateway.user}"
+  }
+
+  arvados_cluster_id   = "ncucu"
+  extra_ansible_groups = ["consul-cluster-delta-hgiarvados"]
+}
+
+module "arvados-keepproxy" {
+  source = "../modules/arvados-keepproxy"
+
+  image = {
+    name = "${var.base_image_name}"
+    user = "${var.base_image_user}"
+  }
+
+  flavour            = "m1.medium"
+  domain             = "hgi.sanger.ac.uk"
+  security_group_ids = "${module.openstack.security_group_ids}"
+  key_pair_ids       = "${module.openstack.key_pair_ids}"
+  network_id         = "${module.openstack.network_id}"
+
+  bastion = {
+    host = "${module.ssh-gateway.host}"
+    user = "${module.ssh-gateway.user}"
+  }
+
+  arvados_cluster_id   = "ncucu"
+  extra_ansible_groups = ["consul-cluster-delta-hgiarvados"]
+}
+
+module "arvados-keep" {
+  source = "../modules/arvados-keep"
+
+  image = {
+    name = "${var.base_image_name}"
+    user = "${var.base_image_user}"
+  }
+
+  count              = 2
+  flavour            = "m1.medium"
+  domain             = "node.hgi-delta.consul"
+  security_group_ids = "${module.openstack.security_group_ids}"
+  key_pair_ids       = "${module.openstack.key_pair_ids}"
+  network_id         = "${module.openstack.network_id}"
+
+  bastion = {
+    host = "${module.ssh-gateway.host}"
+    user = "${module.ssh-gateway.user}"
+  }
+
+  arvados_cluster_id   = "ncucu"
+  extra_ansible_groups = ["consul-cluster-delta-hgiarvados"]
+}
+
+module "arvados-shell" {
+  source = "../modules/arvados-shell"
+
+  image = {
+    name = "${var.base_image_name}"
+    user = "${var.base_image_user}"
+  }
+
+  flavour            = "m1.small"
+  domain             = "hgi.sanger.ac.uk"
+  security_group_ids = "${module.openstack.security_group_ids}"
+  key_pair_ids       = "${module.openstack.key_pair_ids}"
+  network_id         = "${module.openstack.network_id}"
+
+  bastion = {
+    host = "${module.ssh-gateway.host}"
+    user = "${module.ssh-gateway.user}"
+  }
+
+  arvados_cluster_id   = "ncucu"
+  extra_ansible_groups = ["consul-cluster-delta-hgiarvados"]
+}
+
+module "arvados-compute-node" {
+  source = "../modules/arvados-compute-node"
+
+  image = {
+    name = "${var.base_image_name}"
+    user = "${var.base_image_user}"
+  }
+
+  count              = 3
+  flavour            = "m1.xlarge"
+  domain             = "node.hgi-delta.consul"
+  security_group_ids = "${module.openstack.security_group_ids}"
+  key_pair_ids       = "${module.openstack.key_pair_ids}"
+  network_id         = "${module.openstack.network_id}"
+
+  bastion = {
+    host = "${module.ssh-gateway.host}"
+    user = "${module.ssh-gateway.user}"
+  }
+
+  arvados_cluster_id   = "ncucu"
+  extra_ansible_groups = ["consul-cluster-delta-hgiarvados"]
+}
