eaaee3221e6c557511521fffb1bd2518e80965da
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-11-08 15:50:13 +0000

    add warning regarding image names and update freebsd11 image to 575611a5

--- ci/before_scripts.d/10-tf-vars.sh
+++ ci/before_scripts.d/10-tf-vars.sh
@@ -10,6 +10,10 @@ else
     >&2 echo "REGION unset or empty"
 fi
 
+###############################################################################
+# If you change these image_name values, terraform will DESTROY and RE-CREATE 
+# all instances that use them! BE CAREFUL!
+###############################################################################
 export TF_VAR_base_image_name=hgi-base-xenial-d806d486
 export TF_VAR_base_image_user=ubuntu
 
@@ -19,6 +23,6 @@ export TF_VAR_docker_image_user=ubuntu
 export TF_VAR_arvados_base_image_name=hgi-base-jessie-d806d486 
 export TF_VAR_arvados_base_image_user=debian
 
-export TF_VAR_freebsd_base_image_name=hgi-base-freebsd11-8c9ace19
+export TF_VAR_freebsd_base_image_name=hgi-base-freebsd11-575611a5
 export TF_VAR_freebsd_base_image_user=beastie
 
