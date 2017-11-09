68b6b133fb066ec619737b16745b6ab3b517ba30
Sarah Chacko <sc35@sanger.ac.uk>
2017-11-09 16:47:25 +0000

    added spark/hail user em21

---
+++ terraform/delta-hgi/spark-em21.tf
@@ -0,0 +1,45 @@
+module "spark-master-em21" {
+  source = "../modules/spark-master"
+  count  = 1
+
+  image = {
+    name = "${var.base_image_name}"
+    user = "${var.base_image_user}"
+  }
+
+  flavour            = "m1.large"
+  domain             = "hgi.sanger.ac.uk"
+  security_group_ids = "${module.openstack.security_group_ids}"
+  key_pair_ids       = "${module.openstack.key_pair_ids}"
+  network_id         = "${module.openstack.network_id}"
+
+  bastion = {
+    host = "${module.ssh-gateway.host}"
+    user = "${module.ssh-gateway.user}"
+  }
+
+  spark_cluster_id = "em21"
+}
+
+module "spark-compute-em21" {
+  source = "../modules/spark-compute"
+  count  = 2
+
+  image = {
+    name = "${var.base_image_name}"
+    user = "${var.base_image_user}"
+  }
+
+  flavour            = "m1.large"
+  domain             = "hgi.sanger.ac.uk"
+  security_group_ids = "${module.openstack.security_group_ids}"
+  key_pair_ids       = "${module.openstack.key_pair_ids}"
+  network_id         = "${module.openstack.network_id}"
+
+  bastion = {
+    host = "${module.ssh-gateway.host}"
+    user = "${module.ssh-gateway.user}"
+  }
+
+  spark_cluster_id = "em21"
+}
