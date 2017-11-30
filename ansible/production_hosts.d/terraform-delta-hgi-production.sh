86981bd7590e83fb8771e201cf33e7ef24feb813
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-11-30 19:11:45 +0000

    set CONSUL_HTTP_TOKEN in ansible terraform dynamic inventory scripts

--- ansible/production_hosts.d/terraform-delta-hgi-production.sh
+++ ansible/production_hosts.d/terraform-delta-hgi-production.sh
@@ -5,4 +5,6 @@ set -euf -o pipefail
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${SCRIPT_DIRECTORY}/../inventory_scripts/terraform-inventory-functions.sh"
 
+export CONSUL_HTTP_TOKEN="${TERRAFORM_CONSUL_TOKEN}"
+
 terraform_inventory "${SCRIPT_DIRECTORY}/../../terraform/delta-hgi" production $@
