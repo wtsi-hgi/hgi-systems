1d00e8a75001574a9fcf48929f6243fa7b47fb6f
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-11-30 23:50:47 +0000

    Corrects directory common script path.

--- ci/before_scripts.d/80-consul-locks-consul.sh
+++ ci/before_scripts.d/80-consul-locks-consul.sh
@@ -1,4 +1,4 @@
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "${SCRIPT_DIRECTORY}/common.sh"
+source "${SCRIPT_DIRECTORY}/../common.sh"
 
 ensureSet LOCKS_CONSUL_HTTP_TOKEN
