a242cce539cf59150e119314d27ac9402b50e965
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-11-30 12:57:45 +0000

    Sets Terraform Consul token only in Terraform jobs.

--- ci/before_scripts.d/60-terraform-consul.sh
+++ ci/before_scripts.d/60-terraform-consul.sh
@@ -2,7 +2,3 @@ if [[ -z "${TERRAFORM_CONSUL_TOKEN+x}" ]]; then
     >&2 echo "TERRAFORM_CONSUL_TOKEN must be set!"
     exit 1
 fi
-
-# FIXME: it is not possible for jobs to use different tokens with this scheme - `CONSUL_HTTP_TOKEN` should be defined in
-# the job's environment
-export CONSUL_HTTP_TOKEN=${TERRAFORM_CONSUL_TOKEN}
