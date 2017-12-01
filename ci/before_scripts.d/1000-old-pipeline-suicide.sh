3790c2003c86f4d005654aade3342c5d8c29756e
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-12-01 12:54:54 +0000

    Print out some GitLab CI variables.

--- ci/before_scripts.d/1000-old-pipeline-suicide.sh
+++ ci/before_scripts.d/1000-old-pipeline-suicide.sh
@@ -3,4 +3,11 @@ source "${SCRIPT_DIRECTORY}/../common.sh"
 
 ensureSet CI_PIPELINE_ID CI_PROJECT_ID CI_SERVER GITLAB_TOKEN
 
+echo $GITLAB_CI
+echo $CI_SERVER
+echo $CI_PROJECT_URL
+echo $CI_REPOSITORY_URL
+echo $CI_ENVIRONMENT_URL
+echo $CI
+
 ${SCRIPT_DIRECTORY}/../old-pipeline-suicide.py
