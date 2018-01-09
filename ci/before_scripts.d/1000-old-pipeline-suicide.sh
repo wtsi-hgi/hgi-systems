2fcdfb8e15ac40fa86b76b109c93dd2df62ff27d
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-01-09 15:15:10 +0000

    Removes forgotten call to `ensureSet` in pipeline suicide script.

--- ci/before_scripts.d/1000-old-pipeline-suicide.sh
+++ ci/before_scripts.d/1000-old-pipeline-suicide.sh
@@ -1,8 +1,6 @@
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${SCRIPT_DIRECTORY}/../common.sh"
 
-ensureSet CI_PIPELINE_ID CI_PROJECT_ID CI_PROJECT_URL GITLAB_TOKEN
-
 read -a unsetVariables <<< $(getUnset CI_PIPELINE_ID CI_PROJECT_ID CI_PROJECT_URL GITLAB_TOKEN)
 
 if [[ -z ${unsetVariables+x} ]]; then
