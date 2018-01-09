36779f313f8a975f49095d8f8e794c917ede3eff
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-01-09 14:49:25 +0000

    Prevents pipeline suicide if running locally.

--- ci/before_scripts.d/1000-old-pipeline-suicide.sh
+++ ci/before_scripts.d/1000-old-pipeline-suicide.sh
@@ -3,4 +3,11 @@ source "${SCRIPT_DIRECTORY}/../common.sh"
 
 ensureSet CI_PIPELINE_ID CI_PROJECT_ID CI_PROJECT_URL GITLAB_TOKEN
 
-${SCRIPT_DIRECTORY}/../old-pipeline-suicide.py
+read -a unsetVariables <<< $(getUnset CI_PIPELINE_ID CI_PROJECT_ID CI_PROJECT_URL GITLAB_TOKEN)
+
+if [[ -z ${unsetVariables+x} ]]; then
+    ${SCRIPT_DIRECTORY}/../old-pipeline-suicide.py
+else
+    printUnset "${unsetVariables[@]}"
+    >&2 echo "Not contemplating old-pipeline suicide as assuming running outside of the CI environment"
+fi
