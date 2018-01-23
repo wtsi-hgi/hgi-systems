ffa26123440e95d5496991c8ceb7596bc48814be
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-01-23 16:54:40 +0000

    Corrects version show for thriftybuilder.

--- ci/build-docker-images.sh
+++ ci/build-docker-images.sh
@@ -3,6 +3,6 @@
 set -euf -o pipefail
 
 pip install -q git+https://github.com/wtsi-hgi/thrifty-builder#egg=thriftybuilder
-thrifty -v
+pip show thriftybuilder | grep Version
 
 thrifty -vv docker/build-configuration.yml
