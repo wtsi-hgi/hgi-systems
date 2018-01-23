2106687c621992e8387d7183edee120bf4d6cc97
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-01-23 16:58:25 +0000

    Sleep for debug.

--- ci/build-docker-images.sh
+++ ci/build-docker-images.sh
@@ -5,4 +5,6 @@ set -euf -o pipefail
 pip install -q git+https://github.com/wtsi-hgi/thrifty-builder#egg=thriftybuilder
 pip show thriftybuilder | grep Version
 
+sleep 250
+
 thrifty -vv docker/build-configuration.yml
