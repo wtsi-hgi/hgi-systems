6de25f50c79f89565a04f5c5dc78b2d0e45da1c2
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-01-24 15:54:43 +0000

    Store built image checksums in Consul.

--- ci/build-docker-images.sh
+++ ci/build-docker-images.sh
@@ -4,4 +4,8 @@ set -euf -o pipefail
 
 pip show thriftybuilder | grep Version
 
+# FIXME: Stop stealing Consul details designed for use with Consul lock - either make own or generalise!
+export CONSUL_HTTP_TOKEN=${LOCKS_CONSUL_HTTP_TOKEN}
+export CONSUL_HTTP_ADDR=${LOCKS_CONSUL_HTTP_ADDR}
+
 thrifty -vv docker/build-configuration.yml
