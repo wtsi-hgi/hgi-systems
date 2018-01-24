5ae0c6b2ccc7d9b21e754d5846badc4423b15ec4
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-01-24 17:53:35 +0000

    update arvados compute node image to 76f5a90e

--- ci/before_scripts.d/10-tf-vars.sh
+++ ci/before_scripts.d/10-tf-vars.sh
@@ -29,6 +29,6 @@ export TF_VAR_docker_image_user=ubuntu
 export TF_VAR_freebsd_base_image_name=hgi-base-freebsd11-575611a5
 export TF_VAR_freebsd_base_image_user=beastie
 
-export TF_VAR_arvados_compute_node_image_name=hgi-arvados_compute-xenial-54cad3ae
+export TF_VAR_arvados_compute_node_image_name=hgi-arvados_compute-xenial-76f5a90e
 export TF_VAR_arvados_compute_node_image_user=ubuntu
 
