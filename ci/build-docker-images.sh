c9add8eba08a689fa250c8efe082ba12474937e0
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-01-24 12:35:09 +0000

    Switches to using image with Thrifty already installed.

--- ci/build-docker-images.sh
+++ ci/build-docker-images.sh
@@ -2,9 +2,6 @@
 
 set -euf -o pipefail
 
-pip install -q git+https://github.com/wtsi-hgi/thrifty-builder#egg=thriftybuilder
 pip show thriftybuilder | grep Version
-
-sleep 250
 
 thrifty -vv docker/build-configuration.yml
