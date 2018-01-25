5691bff4fbaf10214a78767368db560ab5ec5429
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-01-25 16:11:58 +0000

    Updates Consul CI environment variable name.

--- ci/build-docker-images.sh
+++ ci/build-docker-images.sh
@@ -5,7 +5,7 @@ set -euf -o pipefail
 pip show thriftybuilder | grep Version
 
 # FIXME: Stop stealing Consul details designed for use with Consul lock - either make own or generalise!
-export CONSUL_HTTP_TOKEN=${LOCKS_CONSUL_HTTP_TOKEN}
-export CONSUL_HTTP_ADDR=${LOCKS_CONSUL_HTTP_ADDR}
+export CONSUL_HTTP_TOKEN=${CI_CONSUL_HTTP_TOKEN}
+export CONSUL_HTTP_ADDR=${CI_CONSUL_HTTP_ADDR}
 
 thrifty -vv docker/build-configuration.yml
