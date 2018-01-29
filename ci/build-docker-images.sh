60da2173c33a46cd8c0d5b5ce538e1619a82a5a6
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-01-29 14:13:28 +0000

    Stop old pipeline suicide in image build step as Josh wants to allow out-of-date execution in the later terraform step.

--- ci/build-docker-images.sh
+++ ci/build-docker-images.sh
@@ -6,7 +6,7 @@ SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${SCRIPT_DIRECTORY}/common.sh"
 
 ensureSet CI_CONSUL_HTTP_TOKEN CI_CONSUL_HTTP_ADDR CI_LOCK_PREFIX CI_JOB_ID \
-          CI_PROJECT_ID CI_PROJECT_URL GITLAB_TOKEN CI_PIPELINE_ID \
+          CI_PROJECT_ID CI_PROJECT_URL GITLAB_TOKEN \
           CI_DOCKER_REGISTRY_URL CI_DOCKER_REGISTRY_USERNAME CI_DOCKER_REGISTRY_PASSWORD
 
 echo "thriftybuilder: $(pip show thriftybuilder | grep Version)"
@@ -18,5 +18,4 @@ consul-lock -v execute \
     -i=10 \
     --metadata="{jobId: ${CI_JOB_ID}}" \
     --on-before-lock=ci/release-dead-job-lock.py \
-    --on-before-lock=ci/old-pipeline-suicide.py \
     ${CI_LOCK_PREFIX}/docker-build "thrifty -vv docker/build-configuration.yml"
