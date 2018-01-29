bd2e9a6816cb57034a0c39635d75ce1c78536de5
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-01-29 14:46:09 +0000

    Execute Ansible using consul-lock execute to clean up correctly.

---
+++ ci/ansible-locked.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ANSIBLE_SCRIPT="${SCRIPT_DIRECTORY}/ansible.sh"
+
+source "${SCRIPT_DIRECTORY}/common.sh"
+read -a unsetVariables <<< $(getUnset CI_CONSUL_HTTP_TOKEN CI_CONSUL_HTTP_ADDR CI_JOB_ID ANSIBLE_LOCK_PREFIX CI_JOB_NAME)
+if [[ -z ${unsetVariables+x} ]]; then
+    trap cleanupLock INT TERM
+
+    >&2 echo "Getting Consul lock..."
+    CONSUL_HTTP_TOKEN=${CI_CONSUL_HTTP_TOKEN} CONSUL_HTTP_ADDR=${CI_CONSUL_HTTP_ADDR} consul-lock -v execute \
+         -i=10 \
+         --metadata="{jobId: ${CI_JOB_ID}}" \
+         --on-before-lock=ci/release-dead-job-lock.py \
+         --on-before-lock=ci/old-pipeline-suicide.py \
+         ${ANSIBLE_LOCK_PREFIX}/${CI_JOB_NAME}
+         "${ANSIBLE_SCRIPT}"
+else
+    printUnset "${unsetVariables[@]}"
+    >&2 echo "Continuing without locking as necessary variables are not set"
+    "${ANSIBLE_SCRIPT}"
+fi
