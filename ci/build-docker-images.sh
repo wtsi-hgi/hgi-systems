8c634b15bc04f8d4f84edd3e8ccacaa52689224a
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-01-29 14:09:53 +0000

    Support pipeline suicide and old job lock release on image build job.

--- ci/build-docker-images.sh
+++ ci/build-docker-images.sh
@@ -5,7 +5,8 @@ set -euf -o pipefail
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${SCRIPT_DIRECTORY}/common.sh"
 
-ensureSet CI_CONSUL_HTTP_TOKEN CI_CONSUL_HTTP_ADDR CI_LOCK_PREFIX \
+ensureSet CI_CONSUL_HTTP_TOKEN CI_CONSUL_HTTP_ADDR CI_LOCK_PREFIX CI_JOB_ID \
+          CI_PROJECT_ID CI_PROJECT_URL GITLAB_TOKEN CI_PIPELINE_ID \
           CI_DOCKER_REGISTRY_URL CI_DOCKER_REGISTRY_USERNAME CI_DOCKER_REGISTRY_PASSWORD
 
 echo "thriftybuilder: $(pip show thriftybuilder | grep Version)"
@@ -13,4 +14,9 @@ echo "thriftybuilder: $(pip show thriftybuilder | grep Version)"
 export CONSUL_HTTP_TOKEN=${CI_CONSUL_HTTP_TOKEN}
 export CONSUL_HTTP_ADDR=${CI_CONSUL_HTTP_ADDR}
 
-consul-lock -v execute ${CI_LOCK_PREFIX}/docker-build "thrifty -vv docker/build-configuration.yml"
+consul-lock -v execute \
+    -i=10 \
+    --metadata="{jobId: ${CI_JOB_ID}}" \
+    --on-before-lock=ci/release-dead-job-lock.py \
+    --on-before-lock=ci/old-pipeline-suicide.py \
+    ${CI_LOCK_PREFIX}/docker-build "thrifty -vv docker/build-configuration.yml"
