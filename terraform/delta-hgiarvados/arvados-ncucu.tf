ec8753be9f0f7b1c6f3e1b231afe361875e0809c
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-01-30 14:53:53 +0000

    scale arvados ncucu down to 1 compute node

--- terraform/delta-hgiarvados/arvados-ncucu.tf
+++ terraform/delta-hgiarvados/arvados-ncucu.tf
@@ -123,7 +123,7 @@ module "arvados-shell" {
     user = "${var.base_image_user}"
   }
 
-  flavour            = "m1.small"
+  flavour            = "m1.medium"
   domain             = "hgi.sanger.ac.uk"
   security_group_ids = "${module.openstack.security_group_ids}"
   key_pair_ids       = "${module.openstack.key_pair_ids}"
@@ -146,7 +146,7 @@ module "arvados-compute-node-noconf" {
     user = "${var.arvados_compute_node_image_user}"
   }
 
-  count              = 125
+  count              = 1
   flavour            = "m1.xlarge"
   domain             = "node.delta-hgiarvados.consul"
   security_group_ids = "${module.openstack.security_group_ids}"
