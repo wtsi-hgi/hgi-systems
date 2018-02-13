6e811049beb16694f1afbb496e78205f7f5b1ba2
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-02-13 09:53:19 +0000

    Adds script to search Git repositories for strings that look like commit references.

---
+++ scripts/used-images/reference-search.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+GREP_PATTERN="(^| |-|_|\"|')[0-9a-f]{8}($| |-|_|\"|')"
+
+if [[ "$#" -ne 1 ]]; then
+    >&2 echo "The location of the git repository to examine should be passed as the first and only argument"
+    exit 1
+fi
+repositoryLocation="$1"
+
+cd "${repositoryLocation}"
+
+totalCommits=$(git rev-list --count --all)
+git rev-list --all | (
+    i=0
+    while read commit; do
+        i=$((i + 1))
+        git grep -h -I -P "${GREP_PATTERN}" "${commit}" || true
+        >&2 echo "{\"commit\": ${i}, \"total\": ${totalCommits}}"
+    done
+) | grep --only-matching -E "[0-9a-f]{8}" | sort | uniq
