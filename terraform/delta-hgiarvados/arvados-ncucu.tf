54f5d203bcec0a97870973d08404fa8a4e1fad7b
Joshua Randall <joshua.randall@sanger.ac.uk>
2018-03-07 11:25:15 +0000

    scale arvados ncucu up to 85 compute nodes

--- terraform/delta-hgiarvados/arvados-ncucu.tf
+++ terraform/delta-hgiarvados/arvados-ncucu.tf
@@ -193,7 +193,7 @@ module "arvados-compute-node-noconf" {
     user = "${var.arvados_compute_node_image_user}"
   }
 
-  count              = 20
+  count              = 85
   flavour            = "m1.xlarge"
   domain             = "node.delta-hgiarvados.consul"
   security_group_ids = "${module.openstack.security_group_ids}"
