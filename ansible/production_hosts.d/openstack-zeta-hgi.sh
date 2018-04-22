c23851bda057f08fca4596935aeed452c5f6881a
Mercury <mercury@sanger.ac.uk>
2018-04-22 16:09:12 +0100

    switch bootstrap dynamic inventory to zeta

--- ansible/production_hosts.d/openstack-zeta-hgi.sh
+++ ansible/production_hosts.d/openstack-zeta-hgi.sh
@@ -4,5 +4,5 @@ set -euf -o pipefail
 
 export OS_TENANT_NAME=hgi
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "${SCRIPT_DIRECTORY}/../inventory_scripts/openstack-inventory-delta.sh"
+source "${SCRIPT_DIRECTORY}/../inventory_scripts/openstack-inventory-zeta.sh"
 
