046d2b285e0390f1b9cf867f4edcd94cee7c1ee1
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-04-25 23:21:34 +0000

    move sourcing of before scripts to after git setup and echo before script output to screen with indentation

--- scripts/dev-container/_user-setup.sh
+++ scripts/dev-container/_user-setup.sh
@@ -17,13 +17,6 @@ export OS_AUTH_URL="${DELTA_OS_AUTH_URL}"
 >&2 echo "Setting GitLab specific variables"
 export CI_PROJECT_DIR=/mnt/host/hgi-systems/
 
->&2 echo "Sourcing before scripts"
-(/mnt/host/hgi-systems/ci/source-before-scripts.sh /mnt/host/hgi-systems/ci/before_scripts.d &> /tmp/sourcing.log) \
-    || (echo "Before scripts failed with exit code $0" && cat /tmp/sourcing.log && echo "(Failure above)" && exit 1)
-rm /tmp/sourcing.log
-# XXX: There's inevitably a much better way of coping with errors whilst sourcing than doing it twice!
-. /mnt/host/hgi-systems/ci/source-before-scripts.sh /mnt/host/hgi-systems/ci/before_scripts.d &> /dev/null
-
 >&2 echo "Setup Git"
 # Note: copying the below files so that they can be changed
 cp /mnt/host/.gitconfig ~/.gitconfig
@@ -37,6 +30,13 @@ ln -s /mnt/host/id_rsa ~/.ssh/0-${HOST_USER_NAME}.key
 chmod -R 700 ~/.ssh
 eval $(ssh-agent) > /dev/null 2>&1
 ssh-add ~/.ssh/*.key > /dev/null 2>&1
+
+>&2 echo "Sourcing before scripts"
+(/mnt/host/hgi-systems/ci/source-before-scripts.sh /mnt/host/hgi-systems/ci/before_scripts.d &> /tmp/sourcing.log) \
+    || (echo "Before scripts failed with exit code $0" && cat /tmp/sourcing.log && echo "(Failure above)" && exit 1)
+rm /tmp/sourcing.log
+## XXX: There's inevitably a much better way of coping with errors whilst sourcing than doing it twice!
+. /mnt/host/hgi-systems/ci/source-before-scripts.sh /mnt/host/hgi-systems/ci/before_scripts.d 2>&1 | sed 's/^/    /g'
 
 >&2 echo "Starting shell..."
 bash
