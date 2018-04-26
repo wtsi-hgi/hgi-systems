013d34616b3b85eb965507fd1ef902ac54e33283
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-04-26 20:06:00 +0000

    change zeta-hgi subnet to match delta-hgi

--- terraform/zeta-hgi-core/openstack.tf
+++ terraform/zeta-hgi-core/openstack.tf
@@ -3,8 +3,8 @@ module "openstack" {
   env                   = "${var.env}"
   region                = "${var.region}"
   mercury_keypair       = "${var.mercury_keypair}"
-  subnet                = "10.101.0.0/24"
-  gateway_ip            = "10.101.0.1"
+  subnet                = "10.100.0.0/24"
+  gateway_ip            = "10.100.0.1"
   dns_nameservers       = ["172.18.255.1", "172.18.255.2", "172.18.255.3"]
   host_routes           = []
   router_count          = 1
