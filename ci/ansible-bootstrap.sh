e85fcf9798c4379938350003c849d7c13633bf8c
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-05-02 15:39:05 +0000

    don't include datacenter in ansible-bootstrap consul kv check and don't fail silently

--- ci/ansible-bootstrap.sh
+++ ci/ansible-bootstrap.sh
@@ -11,14 +11,13 @@ ensureSet CI_PROJECT_DIR REGION SETUP ANSIBLE_VAULT_PASSWORD_FILE TERRAFORM_CONS
 # if we've made it this far, gitlab runner must be working
 # check to see if terraform remote state is working
 region_setup="${REGION}-${SETUP}"
-consul_datacenter="${region_setup}"
 echo "Testing terraform remote state for ${region_setup}"
 tstate=$(cd terraform/${region_setup} && (CONSUL_HTTP_TOKEN="${TERRAFORM_CONSUL_TOKEN}" terraform init > /dev/null) && echo "ok" || echo "failed")
 if [[ "${tstate}" == "ok" ]]; then
     echo "Terraform remote state is ok, checking required consul kv data is present and accessible to terraform"
     for key in terraform/consul_cluster_acl_agent_token terraform/consul_encrypt terraform/consul_cluster_acl_agent_token terraform/consul_template_token; do
 	echo -n "Checking for ${key}... "
-	curl -o /dev/null --silent --fail --header "X-Consul-Token: ${TERRAFORM_CONSUL_TOKEN}" "http://consul.${region_setup}.hgi.sanger.ac.uk:8500/v1/kv/${key}?dc=${consul_datacenter}"
+	curl -o /dev/null --fail --header "X-Consul-Token: ${TERRAFORM_CONSUL_TOKEN}" "http://consul.${region_setup}.hgi.sanger.ac.uk:8500/v1/kv/${key}"
 	echo "ok"
     done
     echo "All OK, no need to bootstrap with ansible"
