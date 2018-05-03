942fbbf91db9f88a5641111c9cf62808d8915660
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-05-03 19:45:17 +0000

    configure wan and lan addresses for docker-consul-clusters using newly added consul_members_facts module

--- ansible/library/consul_members_facts.py
+++ ansible/library/consul_members_facts.py
@@ -36,8 +36,22 @@ options:
     description: 
       - Path to the consul command
     required: false
+  wan:
+    description: 
+      - Fetch data for WAN members instead of LAN
+    required: false
+    default: no
+  fields_list_output_key:
+    description:
+      - Output key in which to return a list of fields in the output (default "fields")
+  members_output_key:
+    description:
+      - Output key in which to return a list of dicts (one for each member), each of which contains the fields as map keys.
+  by_field_output_key_prefix:
+    description:
+      - Prefix for an output key in which to return a dict (keyed by field value) of a list (of all members with that field value) of dicts (with the fields for that member).
 requirements:
-  - "python >= 3"
+  - "python >= 3.5"
 """
 
 EXAMPLES = """
@@ -53,12 +67,20 @@ import re
 def main():
     module = AnsibleModule(
         argument_spec={
-            "consul_bin": {"required": False, "default": "/usr/bin/consul", type: "bytes"},
-            "mgmt_token": {"required": False, "default": "", type: "bytes"},
-            "wan": {"required": False, "default": False, type: "boolean"},
+            "consul_bin": {"default": "/usr/bin/consul", "type": "str"},
+            "mgmt_token": {"default": "", "type": "str"},
+            "wan": {"default": "False", "type": "bool"},
+            "fields_list_output_key": {"default": "fields", "type": "str"},
+            "members_output_key": {"default": "members", "type": "str"},
+            "by_field_output_key_prefix": {"default": "members_by_", "type": "str"},
         },
         supports_check_mode=True
     )
+    fields_list_output_key = module.params["fields_list_output_key"]
+    members_output_key = module.params["members_output_key"]
+    by_field_output_key_prefix = module.params["by_field_output_key_prefix"]
+
+    # prepare command line and call consul members
     consul_members_cmdline = [module.params["consul_bin"], "members"]
     if module.params["mgmt_token"] != "":
         consul_members_cmdline.extend(["-token=%s" % (module.params["mgmt_token"])])
@@ -71,36 +93,44 @@ def main():
     except OSError as ose:
         module.fail_json(msg="error running consul command: %s" % (ose))
 
-    consul_members = {}
-
     # parse consul members output
     lines = consul_members_process.stdout.decode('utf-8').splitlines()
     header_line = lines[0]
-    members_lines = lines[1:]
+    member_lines = lines[1:]
 
     # parse header
     column_names = header_line.split()
     column_start_positions = {}
     column_end_positions = {}
-    last_end_position = length(header_line)-1
+    last_end_position = len(header_line)-1
     for column_name in reversed(column_names):
         column_start_position = header_line.find(column_name)
-        if column_position < 0:
+        if column_start_position < 0:
             module.fail_json(msg="consul members output could not be parsed: could not find column for %s" % (column_name))
         column_start_positions[column_name] = column_start_position
         column_end_positions[column_name] = last_end_position
         last_end_position = column_start_position - 1
-    consul_members["fields"] = column_names
+        
+    # initialise output
+    consul_members = {}
+    consul_members[fields_list_output_key] = column_names
+    consul_members[members_output_key] = []
 
     # parse data
-    consul_members["data"] = []
     for member_line in member_lines:
         member_data = {}
         for column_name in column_names:
             column_start_position = column_start_positions[column_name]
             column_end_position = column_end_positions[column_name]
-            member_data[column_name] = member_line[column_start_position:column_end_position]
-        consul_members["data"].push(member_data)
+            member_data[column_name] = member_line[column_start_position:column_end_position].strip()
+        consul_members[members_output_key].append(member_data)
+        for column_name in column_names:
+            by_field_output_key = by_field_output_key_prefix + column_name
+            if by_field_output_key not in consul_members:
+                consul_members[by_field_output_key] = {}
+            if member_data[column_name] not in consul_members[by_field_output_key]:
+                consul_members[by_field_output_key][member_data[column_name]] = []
+            consul_members[by_field_output_key][member_data[column_name]].append(member_data)
 
     module.exit_json(changed=False, message="Facts set from consul members", consul_members=consul_members)
 
