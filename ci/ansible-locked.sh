2d93ef5c4201ee686335a3986e023c1bcb88ab5a
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-05-03 13:40:26 +0100

    Adds more information about Consul keys involved with CI locking.

--- ci/ansible-locked.sh
+++ ci/ansible-locked.sh
@@ -8,13 +8,14 @@ ANSIBLE_SCRIPT="${SCRIPT_DIRECTORY}/ansible.sh"
 source "${SCRIPT_DIRECTORY}/common.sh"
 read -a unsetVariables <<< $(getUnset CI_CONSUL_HTTP_TOKEN CI_CONSUL_HTTP_ADDR CI_JOB_ID ANSIBLE_LOCK_PREFIX CI_JOB_NAME)
 if [[ -z ${unsetVariables+x} ]]; then
-    >&2 echo "Getting Consul lock..."
+    lockKey="${ANSIBLE_LOCK_PREFIX}/${CI_JOB_NAME}"
+    >&2 echo "Getting Consul lock for key: ${lockKey}..."
     CONSUL_HTTP_TOKEN=${CI_CONSUL_HTTP_TOKEN} CONSUL_HTTP_ADDR=${CI_CONSUL_HTTP_ADDR} consul-lock -v execute \
          -i=10 \
          --metadata="{jobId: ${CI_JOB_ID}}" \
          --on-before-lock=ci/release-dead-job-lock.py \
          --on-before-lock=ci/old-pipeline-suicide.py \
-         ${ANSIBLE_LOCK_PREFIX}/${CI_JOB_NAME} \
+         "${lockKey}" \
          "${ANSIBLE_SCRIPT}"
 else
     printUnset "${unsetVariables[@]}"
