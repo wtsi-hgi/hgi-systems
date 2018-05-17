dede868816c755412a4217ce75a2b1fd5414d997
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-05-17 13:07:30 +0000

    add terraform-zeta-hgiarvados-wlly8[-core] ansible inventories

---
+++ ansible/inventories/production/terraform-zeta-hgiarvados-wlly8-core-production.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${SCRIPT_DIRECTORY}/../../inventory_scripts/terraform-inventory-functions.sh"
+
+export CONSUL_HTTP_TOKEN="${TERRAFORM_CONSUL_TOKEN}"
+
+terraform_inventory "${SCRIPT_DIRECTORY}/../../../terraform/zeta-hgiarvados-wlly8-core" production $@
---
+++ ansible/inventories/zeta-terraform-hgiarvados-wlly8-core-production/terraform-zeta-hgiarvados-wlly8-core-production.sh
@@ -0,0 +1,1 @@
+../production/terraform-zeta-hgiarvados-wlly8-core-production.sh
