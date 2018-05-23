4ed1adf3eed45cdecc6bf6dd1e522a865974342b
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-05-23 18:06:41 +0000

    add three extra routers for each of the three s3 gateways

--- terraform/modules/openstack-v2/output.tf
+++ terraform/modules/openstack-v2/output.tf
@@ -4,10 +4,13 @@ output "context" {
       security_groups = "${local.security_groups}"
       keypairs        = "${local.keypairs}"
       networks        = "${local.networks}"
+      subnets         = "${local.subnets}"
     }
 
     strings = {
-      floatingip_pool_name = "${var.floatingip_pool_name}"
+      floatingip_pool_name  = "${var.floatingip_pool_name}"
+      external_network_name = "${var.external_network_name}"
+      external_network_id   = "${data.openstack_networking_network_v2.external_network.id}"
     }
 
     lists = {
