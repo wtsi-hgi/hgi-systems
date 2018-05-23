fd1bec0cf755ac9f77d50ba608235627b2d1b818
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-05-23 18:07:01 +0000

    Revert "add three extra routers for each of the three s3 gateways"
    This reverts commit 4ed1adf3eed45cdecc6bf6dd1e522a865974342b.

--- terraform/modules/openstack-v2/main.tf
+++ terraform/modules/openstack-v2/main.tf
@@ -56,10 +56,6 @@ locals {
   networks = {
     main = "${openstack_networking_network_v2.main.id}"
   }
-
-  subnets = {
-    main = "${openstack_networking_subnet_v2.main.id}"
-  }
 }
 
 resource "openstack_networking_subnet_v2" "main" {
@@ -84,85 +80,4 @@ resource "openstack_networking_router_interface_v2" "main_ext" {
   provider  = "openstack"
   router_id = "${openstack_networking_router_v2.main_ext.id}"
   subnet_id = "${openstack_networking_subnet_v2.main.id}"
-}
-
-resource "openstack_networking_router_v2" "main_ext_cog12" {
-  count               = "${var.router_count}"
-  provider            = "openstack"
-  name                = "main_ext_cog12_${var.region}_${var.setup}_${var.env}"
-  external_network_id = "${data.openstack_networking_network_v2.external_network.id}"
-}
-
-resource "openstack_networking_router_interface_v2" "main_ext_cog12" {
-  provider  = "openstack"
-  router_id = "${openstack_networking_router_v2.main_ext_cog12.id}"
-  port_id   = "${openstack_networking_port_v2.main_ext_cog12.id}"
-}
-
-resource "openstack_networking_port_v2" "main_ext_cog12" {
-  name               = "main_ext_cog12_${var.region}_${var.setup}_${var.env}"
-  admin_state_up     = "true"
-  network_id         = "${openstack_networking_network_v2.main.id}"
-  no_security_groups = "true"
-}
-
-resource "openstack_networking_router_route_v2" "main_ext_cog12" {
-  depends_on       = ["openstack_networking_router_interface_v2.main_ext_cog12"]
-  router_id        = "${openstack_networking_router_v2.main_ext.id}"
-  destination_cidr = "172.27.6.12/32"
-  next_hop         = "${openstack_networking_port_v2.main_ext_cog12.all_fixed_ips[0]}"
-}
-
-resource "openstack_networking_router_v2" "main_ext_cog15" {
-  count               = "${var.router_count}"
-  provider            = "openstack"
-  name                = "main_ext_cog15_${var.region}_${var.setup}_${var.env}"
-  external_network_id = "${data.openstack_networking_network_v2.external_network.id}"
-}
-
-resource "openstack_networking_router_interface_v2" "main_ext_cog15" {
-  provider  = "openstack"
-  router_id = "${openstack_networking_router_v2.main_ext_cog15.id}"
-  port_id   = "${openstack_networking_port_v2.main_ext_cog15.id}"
-}
-
-resource "openstack_networking_port_v2" "main_ext_cog15" {
-  name               = "main_ext_cog15_${var.region}_${var.setup}_${var.env}"
-  admin_state_up     = "true"
-  network_id         = "${openstack_networking_network_v2.main.id}"
-  no_security_groups = "true"
-}
-
-resource "openstack_networking_router_route_v2" "main_ext_cog15" {
-  depends_on       = ["openstack_networking_router_interface_v2.main_ext_cog15"]
-  router_id        = "${openstack_networking_router_v2.main_ext.id}"
-  destination_cidr = "172.27.6.15/32"
-  next_hop         = "${openstack_networking_port_v2.main_ext_cog15.all_fixed_ips[0]}"
-}
-
-resource "openstack_networking_router_v2" "main_ext_cog18" {
-  count               = "${var.router_count}"
-  provider            = "openstack"
-  name                = "main_ext_cog18_${var.region}_${var.setup}_${var.env}"
-  external_network_id = "${data.openstack_networking_network_v2.external_network.id}"
-}
-
-resource "openstack_networking_router_interface_v2" "main_ext_cog18" {
-  provider  = "openstack"
-  router_id = "${openstack_networking_router_v2.main_ext_cog18.id}"
-  port_id   = "${openstack_networking_port_v2.main_ext_cog18.id}"
-}
-
-resource "openstack_networking_port_v2" "main_ext_cog18" {
-  name               = "main_ext_cog18_${var.region}_${var.setup}_${var.env}"
-  admin_state_up     = "true"
-  network_id         = "${openstack_networking_network_v2.main.id}"
-  no_security_groups = "true"
-}
-
-resource "openstack_networking_router_route_v2" "main_ext_cog18" {
-  depends_on       = ["openstack_networking_router_interface_v2.main_ext_cog18"]
-  router_id        = "${openstack_networking_router_v2.main_ext.id}"
-  destination_cidr = "172.27.6.18/32"
-  next_hop         = "${openstack_networking_port_v2.main_ext_cog18.all_fixed_ips[0]}"
 }
--- terraform/modules/openstack-v2/output.tf
+++ terraform/modules/openstack-v2/output.tf
@@ -4,13 +4,10 @@ output "context" {
       security_groups = "${local.security_groups}"
       keypairs        = "${local.keypairs}"
       networks        = "${local.networks}"
-      subnets         = "${local.subnets}"
     }
 
     strings = {
-      floatingip_pool_name  = "${var.floatingip_pool_name}"
-      external_network_name = "${var.external_network_name}"
-      external_network_id   = "${data.openstack_networking_network_v2.external_network.id}"
+      floatingip_pool_name = "${var.floatingip_pool_name}"
     }
 
     lists = {
