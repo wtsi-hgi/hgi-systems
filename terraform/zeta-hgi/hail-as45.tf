2321f568427c3815c5e63560caf4007508b44a79
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-05-29 11:53:58 +0100

    Creates hail instances for as45 and ks20 on Zeta.

---
+++ terraform/zeta-hgi/hail-as45.tf
@@ -0,0 +1,30 @@
+module "hail-master-as45" {
+  source            = "../modules/hail/master/hail-master-v2"
+  hail_cluster_id   = "as45"
+  count             = 1
+  env               = "${var.env}"
+  region            = "${var.region}"
+  setup             = "${var.setup}"
+  core_context      = "${data.terraform_remote_state.hgi-core.core_context}"
+  domain            = "hgi.sanger.ac.uk"
+  image             = "${data.terraform_remote_state.hgi-core.hgi-openstack-image-hgi-docker-xenial-4cb02ffa}"
+  network_name      = "main"
+  ssh_gateway       = "${data.terraform_remote_state.hgi-core.ssh_gateway}"
+  flavour           = "o1.large"
+  volume_size_gb    = 100
+}
+
+module "hail-compute-as45" {
+  source            = "../modules/hail/compute/hail-compute-v2"
+  hail_cluster_id   = "as45"
+  count             = 5
+  env               = "${var.env}"
+  region            = "${var.region}"
+  setup             = "${var.setup}"
+  core_context      = "${data.terraform_remote_state.hgi-core.core_context}"
+  domain            = "hgi.sanger.ac.uk"
+  image             = "${data.terraform_remote_state.hgi-core.hgi-openstack-image-hgi-docker-xenial-4cb02ffa}"
+  network_name      = "main"
+  ssh_gateway       = "${data.terraform_remote_state.hgi-core.ssh_gateway}"
+  flavour           = "o1.large"
+}
---
+++ terraform/zeta-hgi/hail-ks20.tf
@@ -0,0 +1,30 @@
+module "hail-master-ks20" {
+  source            = "../modules/hail/master/hail-master-v2"
+  hail_cluster_id   = "ks20"
+  count             = 1
+  env               = "${var.env}"
+  region            = "${var.region}"
+  setup             = "${var.setup}"
+  core_context      = "${data.terraform_remote_state.hgi-core.core_context}"
+  domain            = "hgi.sanger.ac.uk"
+  image             = "${data.terraform_remote_state.hgi-core.hgi-openstack-image-hgi-docker-xenial-4cb02ffa}"
+  network_name      = "main"
+  ssh_gateway       = "${data.terraform_remote_state.hgi-core.ssh_gateway}"
+  flavour           = "o1.large"
+  volume_size_gb    = 20
+}
+
+module "hail-compute-ks20" {
+  source            = "../modules/hail/compute/hail-compute-v2"
+  hail_cluster_id   = "ks20"
+  count             = 0
+  env               = "${var.env}"
+  region            = "${var.region}"
+  setup             = "${var.setup}"
+  core_context      = "${data.terraform_remote_state.hgi-core.core_context}"
+  domain            = "hgi.sanger.ac.uk"
+  image             = "${data.terraform_remote_state.hgi-core.hgi-openstack-image-hgi-docker-xenial-4cb02ffa}"
+  network_name      = "main"
+  ssh_gateway       = "${data.terraform_remote_state.hgi-core.ssh_gateway}"
+  flavour           = "o1.large"
+}
