c1ae5fbd619f654fbfe316c845574d8abe8b1936
Joshua Randall <jr17@sanger.ac.uk>
2018-06-13 17:52:52 +0100

    change from kill -SIGUSR1 (which is not graceful as of passenger v5) to passenger-config detach-process (which is)

--- ansible/roles/arvados-master/files/arvados_api_fat_passenger_killer.sh
+++ ansible/roles/arvados-master/files/arvados_api_fat_passenger_killer.sh
@@ -7,5 +7,5 @@ echo "arvados_api_fat_passenger_killer running at ${date}" >> /var/log/arvados_a
 fat_passenger_pids=$(/usr/sbin/passenger-memory-stats | grep 'Passenger AppPreloader: /var/www/arvados-api/current' | grep -v grep | awk '$4>5000 {print $4, $1}' | sort -rg | awk '{print $2}') 2> /dev/null
 for fat_passenger_pid in ${fat_passenger_pids}; do
     echo "killing ${fat_passenger_pid}"
-    kill -SIGUSR1 ${fat_passenger_pid}
+    passenger-config detach-process ${fat_passenger_pid}
 done >> /var/log/arvados_api_fat_passenger_killer.log
