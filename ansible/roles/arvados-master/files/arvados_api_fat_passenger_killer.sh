eca73701338911bc9183898f8521d7ff6c70782a
Joshua Randall <joshua.randall@sanger.ac.uk>
2018-07-14 04:17:07 +0100

    activate fat passenger killer at 1GB rather than 5GB

--- ansible/roles/arvados-master/files/arvados_api_fat_passenger_killer.sh
+++ ansible/roles/arvados-master/files/arvados_api_fat_passenger_killer.sh
@@ -4,7 +4,7 @@ export PASSENGER_INSTANCE_REGISTRY_DIR=/var/run/nginx/passenger
 
 date=$(date)
 echo "arvados_api_fat_passenger_killer running at ${date}" >> /var/log/arvados_api_fat_passenger_killer.log
-fat_passenger_pids=$(/usr/sbin/passenger-memory-stats | grep 'Passenger AppPreloader: /var/www/arvados-api/current' | grep -v grep | awk '$4>5000 {print $4, $1}' | sort -rg | awk '{print $2}') 2> /dev/null
+fat_passenger_pids=$(/usr/sbin/passenger-memory-stats | grep 'Passenger AppPreloader: /var/www/arvados-api/current' | grep -v grep | awk '$4>1000 {print $4, $1}' | sort -rg | awk '{print $2}') 2> /dev/null
 for fat_passenger_pid in ${fat_passenger_pids}; do
     echo "killing ${fat_passenger_pid}"
     passenger-config detach-process ${fat_passenger_pid}
