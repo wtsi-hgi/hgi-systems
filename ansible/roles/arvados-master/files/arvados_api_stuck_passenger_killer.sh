3831dd064e70eb71bcbebb4cc23f4f18e1e938aa
Joshua Randall <joshua.randall@sanger.ac.uk>
2018-07-14 12:18:45 +0100

    add missing stuck passenger killer

---
+++ ansible/roles/arvados-master/files/arvados_api_stuck_passenger_killer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+date=$(date)
+echo "arvados_api_stuck_passenger_killer running at ${date}" >> /var/log/arvados_api_stuck_passenger_killer.log
+
+stuck_passenger_pids=$(passenger-status --show=xml | python <(echo -e "import xmltodict\nimport json\nimport sys\nprint(json.dumps(xmltodict.parse(sys.stdin.read())))")     | jq -r '.info.supergroups.supergroup.group.processes.process[] | select(.enabled == "DETACHED") | select(.uptime | split("m")[0] > 10) | .pid' | perl -p -e 's/[[:space:]]+/ /g')
+
+if [[ -n "${stuck_passenger_pids}" ]]; then
+    echo "Killing stuck passengers: ${stuck_passenger_pids}" >> /var/log/arvados_api_stuck_passenger_killer.log
+    kill -9 ${stuck_passenger_pids}
+fi
