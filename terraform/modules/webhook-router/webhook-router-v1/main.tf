cb169b26199a80b6a60fb941cb9ff03ca9930aa2
ThomasHickman <Thomas.Hickman42@gmail.com>
2018-07-20 13:54:12 +0100

    Add webhook-master resource

--- terraform/modules/webhook-router/webhook-router-v1/main.tf
+++ terraform/modules/webhook-router/webhook-router-v1/main.tf
@@ -38,10 +38,11 @@ locals {
     "webhook-routers",
   ]
 
-  hostname_format = "webhook-router-%02d"
+  router_hostname_format = "webhook-router-%02d"
+  master_hostname_format = "webhook-master"
 }
 
-module "hgi-openstack-instance" {
+module "webhook-router" {
   source               = "../../hgi-openstack-instance/v1"
   env                  = "${var.env}"
   region               = "${var.region}"
@@ -49,10 +50,10 @@ module "hgi-openstack-instance" {
   core_context         = "${var.core_context}"
   count                = "${var.count}"
   floating_ip_p        = true
-  name_format          = "${local.hostname_format}"
+  name_format          = "${local.router_hostname_format}"
   domain               = "${var.domain}"
-  flavour              = "${var.flavour}"
-  hostname_format      = "${local.hostname_format}"
+  flavour              = "${var.router_flavour}"
+  hostname_format      = "${local.router_hostname_format}"
   ssh_gateway          = "${var.ssh_gateway}"
   keypair_name         = "${var.keypair_name}"
   network_name         = "${var.network_name}"
@@ -62,7 +63,33 @@ module "hgi-openstack-instance" {
   security_group_names = [
     "ping",
     "ssh",
-    "http",
+    "webhook-router",
+  ]
+
+  ansible_groups = "${distinct(concat(local.ansible_groups, var.extra_ansible_groups))}"
+}
+
+module "webhook-master" {
+  source          = "../../hgi-openstack-instance/v1"
+  env             = "${var.env}"
+  region          = "${var.region}"
+  setup           = "${var.setup}"
+  core_context    = "${var.core_context}"
+  count           = 1
+  floating_ip_p   = true
+  name_format     = "${local.master_hostname_format}"
+  domain          = "${var.domain}"
+  flavour         = "${var.master_flavour}"
+  hostname_format = "${local.master_hostname_format}"
+  ssh_gateway     = "${var.ssh_gateway}"
+  keypair_name    = "${var.keypair_name}"
+  network_name    = "${var.network_name}"
+  image           = "${var.image}"
+
+  security_group_names = [
+    "ping",
+    "ssh",
+    "https",
   ]
 
   ansible_groups = "${distinct(concat(local.ansible_groups, var.extra_ansible_groups))}"
