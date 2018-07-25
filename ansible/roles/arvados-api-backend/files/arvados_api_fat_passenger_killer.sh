6d27f191b1742146b1ff5a6c3a3627caee6e65a6
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-07-25 13:09:48 +0100

    update fat/stuck passenger killers

--- ansible/roles/arvados-api-backend/files/arvados_api_fat_passenger_killer.sh
+++ ansible/roles/arvados-api-backend/files/arvados_api_fat_passenger_killer.sh
@@ -8,6 +8,5 @@ echo "arvados_api_fat_passenger_killer running at ${date}" >> /var/log/arvados_a
 fat_passenger_pids=$(/usr/sbin/passenger-memory-stats | grep 'Passenger AppPreloader: /var/www/arvados-api/current' | grep -v grep | awk '$4>600 {print $4, $1}' | sort -rg | awk '{print $2}') 2> /dev/null
 for fat_passenger_pid in ${fat_passenger_pids}; do
     echo "killing ${fat_passenger_pid}"
-    passenger-config detach-process ${fat_passenger_pid}
-    date +"%s" > "/var/www/arvados-api/current/tmp/fat_passengers/${fat_passenger_pid}.timestamp"
+    passenger-config detach-process ${fat_passenger_pid} && date +"%s" > "/var/www/arvados-api/current/tmp/fat_passengers/${fat_passenger_pid}.timestamp"
 done >> /var/log/arvados_api_fat_passenger_killer.log
--- ansible/roles/arvados-api-backend/files/arvados_api_stuck_passenger_killer.sh
+++ ansible/roles/arvados-api-backend/files/arvados_api_stuck_passenger_killer.sh
@@ -13,14 +13,17 @@ find "/var/www/arvados-api/current/tmp/fat_passengers" -name \*.timestamp | whil
     fts=$(cat ${fpts})
     cts=$(date +"%s")
     if [[ -d /proc/${pid} ]]; then
-	cat /proc/48812/cmdline | grep '^Passenger' || (echo "${pid} is not a passenger process" >> /var/log/arvados_api_stuck_passenger_killer.log; rm ${fpts}; continue)
-	echo "fat passenger ${pid} appears to still be running" >> /var/log/arvados_api_stuck_passenger_killer.log
-	tsk=$((${cts}-${fts}))
-	if [[ ${tsk} -gt 300 ]]; then
-	    echo "fat passenger ${pid} has been running for more than 300s, killing it" >> /var/log/arvados_api_stuck_passenger_killer.log
-	    kill -9 ${pid}
-	else
-	    echo "fat passenger ${pid} has only been running for ${tsk}s since becoming fat. not killing it yet" >> /var/log/arvados_api_stuck_passenger_killer.log
-	fi
+        cat /proc/48812/cmdline | grep '^Passenger' || (echo "${pid} is not a passenger process" >> /var/log/arvados_api_stuck_passenger_killer.log; rm ${fpts}; continue)
+        echo "fat passenger ${pid} appears to still be running" >> /var/log/arvados_api_stuck_passenger_killer.log
+        tsk=$((${cts}-${fts}))
+        if [[ ${tsk} -gt 300 ]]; then
+            echo "fat passenger ${pid} has been running for more than 300s, killing it" >> /var/log/arvados_api_stuck_passenger_killer.log
+            kill -9 ${pid}
+        else
+            echo "fat passenger ${pid} has only been running for ${tsk}s since becoming fat. not killing it yet" >> /var/log/arvados_api_stuck_passenger_killer.log
+        fi
+    else
+        echo "fat passenger ${pid} no longer running, removing ${fpts}"
+        rm ${fpts}
     fi
 done
