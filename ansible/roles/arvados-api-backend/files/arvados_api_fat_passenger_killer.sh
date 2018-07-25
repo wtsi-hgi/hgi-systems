1349f450e23da0c6d0f88715a8c00dc6ecbe2b21
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-07-25 12:53:34 +0100

    adjust fat/stuck passenger killers to only murder fat passengers if they keep running for 300s after becoming fat

--- ansible/roles/arvados-api-backend/files/arvados_api_fat_passenger_killer.sh
+++ ansible/roles/arvados-api-backend/files/arvados_api_fat_passenger_killer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export PASSENGER_INSTANCE_REGISTRY_DIR=/var/run/nginx/passenger
+mkdir -p /var/www/arvados-api/current/tmp/fat_passengers
 
 date=$(date)
 echo "arvados_api_fat_passenger_killer running at ${date}" >> /var/log/arvados_api_fat_passenger_killer.log
@@ -8,4 +9,5 @@ fat_passenger_pids=$(/usr/sbin/passenger-memory-stats | grep 'Passenger AppPrelo
 for fat_passenger_pid in ${fat_passenger_pids}; do
     echo "killing ${fat_passenger_pid}"
     passenger-config detach-process ${fat_passenger_pid}
+    date +"%s" > "/var/www/arvados-api/current/tmp/fat_passengers/${fat_passenger_pid}.timestamp"
 done >> /var/log/arvados_api_fat_passenger_killer.log
--- ansible/roles/arvados-api-backend/files/arvados_api_stuck_passenger_killer.sh
+++ ansible/roles/arvados-api-backend/files/arvados_api_stuck_passenger_killer.sh
@@ -3,12 +3,24 @@
 export PASSENGER_INSTANCE_REGISTRY_DIR=/var/run/nginx/passenger
 export PATH=/usr/sbin:$PATH
 
+mkdir -p /var/www/arvados-api/current/tmp/fat_passengers
+
 date=$(date)
 echo "arvados_api_stuck_passenger_killer running at ${date}" >> /var/log/arvados_api_stuck_passenger_killer.log
 
-stuck_passenger_pids=$(passenger-status --show=xml | python <(echo -e "import xmltodict\nimport json\nimport sys\nprint(json.dumps(xmltodict.parse(sys.stdin.read())))")     | jq -r '.info.supergroups.supergroup.group.processes.process[] | select(.enabled == "DETACHED") | select(.uptime | split("m")[0] > 4) | .pid' | perl -p -e 's/[[:space:]]+/ /g')
-
-if [[ -n "${stuck_passenger_pids}" ]]; then
-    echo "Killing stuck passengers: ${stuck_passenger_pids}" >> /var/log/arvados_api_stuck_passenger_killer.log
-    kill -9 ${stuck_passenger_pids}
-fi
+find "/var/www/arvados-api/current/tmp/fat_passengers" -name \*.timestamp | while read fpts; do
+    pid=$(echo ${fpts} | cut -f1 -d.)
+    fts=$(cat ${fpts})
+    cts=$(date +"%s")
+    if [[ -d /proc/${pid} ]]; then
+	cat /proc/48812/cmdline | grep '^Passenger' || (echo "${pid} is not a passenger process" >> /var/log/arvados_api_stuck_passenger_killer.log; rm ${fpts}; continue)
+	echo "fat passenger ${pid} appears to still be running" >> /var/log/arvados_api_stuck_passenger_killer.log
+	tsk=$((${cts}-${fts}))
+	if [[ ${tsk} -gt 300 ]]; then
+	    echo "fat passenger ${pid} has been running for more than 300s, killing it" >> /var/log/arvados_api_stuck_passenger_killer.log
+	    kill -9 ${pid}
+	else
+	    echo "fat passenger ${pid} has only been running for ${tsk}s since becoming fat. not killing it yet" >> /var/log/arvados_api_stuck_passenger_killer.log
+	fi
+    fi
+done
