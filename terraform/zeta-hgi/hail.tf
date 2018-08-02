8ec94fbc69a7fd4462cdb07316fd2073b074072b
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-08-02 08:23:15 +0000

    Upgrade master node to match workers.

--- terraform/zeta-hgi/hail.tf
+++ terraform/zeta-hgi/hail.tf
@@ -34,7 +34,7 @@ module "hail-cluster-as45-2" {
   compute_count          = 5
   master_image           = "${data.terraform_remote_state.hgi-core.hgi-openstack-image-hgi-docker-xenial-4cb02ffa}"
   compute_image          = "${data.terraform_remote_state.hgi-core.hgi-openstack-image-hgi-docker-xenial-4cb02ffa}"
-  master_flavour         = "o1.large"
+  master_flavour         = "o1.xlarge"
   compute_flavour        = "o1.xlarge"
   master_volume_size_gb  = 100
   compute_volume_size_gb = 100
