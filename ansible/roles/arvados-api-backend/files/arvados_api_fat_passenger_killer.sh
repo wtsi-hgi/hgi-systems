09ed1cbde8725d1258191d9f5e4da8efe2593441
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-08-03 17:12:09 +0100

    decrease fat passenger killer definition of fat to 200MB

--- ansible/roles/arvados-api-backend/files/arvados_api_fat_passenger_killer.sh
+++ ansible/roles/arvados-api-backend/files/arvados_api_fat_passenger_killer.sh
@@ -5,7 +5,7 @@ mkdir -p /var/www/arvados-api/current/tmp/fat_passengers
 
 date=$(date)
 echo "arvados_api_fat_passenger_killer running at ${date}" >> /var/log/arvados_api_fat_passenger_killer.log
-fat_passenger_pids=$(/usr/sbin/passenger-memory-stats | grep 'Passenger AppPreloader: /var/www/arvados-api/current' | grep -v grep | awk '$4>500 {print $4, $1}' | sort -rg | awk '{print $2}') 2> /dev/null
+fat_passenger_pids=$(/usr/sbin/passenger-memory-stats | grep 'Passenger AppPreloader: /var/www/arvados-api/current' | grep -v grep | awk '$4>200 {print $4, $1}' | sort -rg | awk '{print $2}') 2> /dev/null
 for fat_passenger_pid in ${fat_passenger_pids}; do
     echo "killing ${fat_passenger_pid}"
     passenger-config detach-process ${fat_passenger_pid} && date +"%s" > "/var/www/arvados-api/current/tmp/fat_passengers/${fat_passenger_pid}.timestamp"
