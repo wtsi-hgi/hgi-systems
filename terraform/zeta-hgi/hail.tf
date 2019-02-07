7c0bbbf4386c78354c3ab67f7048d9b9e029df23
vviyer <vivek@unentschlossen.com>
2019-02-07 12:18:55 +0000

    modify tf file and add new ansible vars file for new cluster vvi2

--- terraform/zeta-hgi/hail.tf
+++ terraform/zeta-hgi/hail.tf
@@ -123,3 +123,24 @@ module "hail-cluster-vvi" {
   master_volume_size_gb  = 100
   compute_volume_size_gb = 100
 }
+
+module "hail-cluster-vvi2" {
+  source       = "../modules/hail/v2/cluster"
+  env          = "${var.env}"
+  region       = "${var.region}"
+  setup        = "${var.setup}"
+  core_context = "${data.terraform_remote_state.hgi-core.core_context}"
+  ssh_gateway  = "${data.terraform_remote_state.hgi-core.ssh_gateway}"
+  domain       = "hgi.sanger.ac.uk"
+  network_name = "main"
+
+  hail_cluster_id        = "vvi"
+  master_count           = 1
+  compute_count          = 2
+  master_image           = "${data.terraform_remote_state.hgi-core.hgi-openstack-image-hgi-docker-xenial-4cb02ffa}"
+  compute_image          = "${data.terraform_remote_state.hgi-core.hgi-openstack-image-hgi-docker-xenial-4cb02ffa}"
+  master_flavour         = "o1.3xlarge"
+  compute_flavour        = "o1.3xlarge"
+  master_volume_size_gb  = 100
+  compute_volume_size_gb = 100
+}
