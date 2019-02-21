480af1afe3b14dec0984dd015aabd4af86fbea7c
vviyer <vivek@unentschlossen.com>
2019-02-21 09:52:02 +0000

    create as45-4 cluster with 40 m1.medium nodes

--- terraform/zeta-hgi/hail.tf
+++ terraform/zeta-hgi/hail.tf
@@ -40,6 +40,28 @@ module "hail-cluster-as45-2" {
   compute_volume_size_gb = 100
 }
 
+module "hail-cluster-as45-4" {
+ source       = "../modules/hail/v2/cluster"
+ env          = "${var.env}"
+ region       = "${var.region}"
+ setup        = "${var.setup}"
+ core_context = "${data.terraform_remote_state.hgi-core.core_context}"
+ ssh_gateway  = "${data.terraform_remote_state.hgi-core.ssh_gateway}"
+ domain       = "hgi.sanger.ac.uk"
+ network_name = "main"
+
+ hail_cluster_id        = "as45-4"
+ master_count           = 1
+ compute_count          = 40
+ master_image           = "${data.terraform_remote_state.hgi-core.hgi-openstack-image-hgi-docker-xenial-4cb02ffa}"
+ compute_image          = "${data.terraform_remote_state.hgi-core.hgi-openstack-image-hgi-docker-xenial-4cb02ffa}"
+ master_flavour         = "m1.medium"
+ compute_flavour        = "m1.medium"
+ master_volume_size_gb  = 100
+ compute_volume_size_gb = 100
+ compute_auto_anti_affinity_p = false
+}
+
 module "hail-cluster-kk8" {
   source       = "../modules/hail/v2/cluster"
   env          = "${var.env}"
